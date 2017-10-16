06d942a9e2a2cd5bd7449bcec023e2b5c428b2c4
Colin Stryker <colin.stryker@gmail.com>
2017-10-16 12:10:41 -0700

    didnt add this file for some reason

---
+++ api_server/services/api/modules/streams/errors.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const ERRORS = {
+	'invalid_stream_type': {
+		code: 'STRM-1000',
+		message: 'Invalid stream type',
+	},
+	'name_required': {
+		code: 'STRM-1001',
+		message: 'Channel type streams must have a name'
+	},
+	'repo_id_required': {
+		code: 'STRM-1002',
+		message: 'File type streams must have a repo_id'
+	},
+	'file_required': {
+		code: 'STRM-1003',
+		message: 'File type streams must have a file'
+	}
+};
+
+module.exports = ERRORS;
