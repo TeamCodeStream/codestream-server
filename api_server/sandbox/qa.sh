d09e44adc3bcab0f75cacd76dee730b172a06149
James Price <jimmyjayp@gmail.com>
2017-11-14 09:18:03 -0500

    In preparation to install API service for QA where mongo is on a dedicated instance

---
+++ api_server/sandbox/qa.sh
@@ -0,0 +1,10 @@
+
+. $CS_API_TOP/sandbox/defaults.sh
+
+export CS_API_MONGO_HOST=qa-mongo.codestream.us
+export CS_API_MONGO_PORT=27017
+export CS_API_SSL_KEYFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-key
+export CS_API_SSL_CERTFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-crt
+export CS_API_SSL_CAFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-ca
+export CS_API_PORT=9443
+export CS_API_LOG_CONSOLE_OK=
