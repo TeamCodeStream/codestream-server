3b76b0b29753f406ff1bf65302cce38e363922bb
James Price <jimmyjayp@gmail.com>
2017-11-15 20:57:16 -0500

    QA points to qm1 as its mongo server

--- api_server/sandbox/qa.sh
+++ api_server/sandbox/qa.sh
@@ -1,7 +1,7 @@
 
 . $CS_API_TOP/sandbox/defaults.sh
 
-export CS_API_MONGO_HOST=qa-mongo.codestream.us
+export CS_API_MONGO_HOST=qm1.codestream.us
 export CS_API_MONGO_PORT=27017
 export CS_API_SSL_KEYFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-key
 export CS_API_SSL_CERTFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-crt
