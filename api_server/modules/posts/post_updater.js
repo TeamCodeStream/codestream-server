6aa2233ab92d48b3605d1d6352c34ada37f7c5e6
Colin Stryker <colin@codestream.com>
2018-01-18 15:41:48 -0800

    remove warns

--- api_server/modules/posts/post_updater.js
+++ api_server/modules/posts/post_updater.js
@@ -40,7 +40,6 @@ class PostUpdater extends ModelUpdater {
 
     // get the post
     getPost (callback) {
-console.warn('GETTING POST ' + this.attributes._id);
         this.request.data.posts.getById(
             this.attributes._id,
             (error, post) => {
@@ -48,7 +47,6 @@ console.warn('GETTING POST ' + this.attributes._id);
                 if (!post) {
                     return callback(this.errorHandler.error('notFound', { info: 'post' }));
                 }
-console.warn('GOT', post.attributes);
                 this.post = post;
                 callback();
             }
