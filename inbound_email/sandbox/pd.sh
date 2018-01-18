6e78fea2d11b9da81af29b1cfabb332a0435e8c1
James Price <jimmyjayp@gmail.com>
2018-01-18 18:46:28 -0500

    A few misconfigurations for PD

--- inbound_email/sandbox/pd.sh
+++ inbound_email/sandbox/pd.sh
@@ -1,5 +1,6 @@
 
 . $CS_MAILIN_TOP/sandbox/defaults.sh
-export CS_MAILIN_REPLY_TO_DOMAIN=dev.codestream.com
+export CS_MAILIN_REPLY_TO_DOMAIN=pd.codestream.com
 export CS_MAILIN_API_HOST=tca3.codestream.us
 export CS_MAILIN_API_PORT=9443
+export CS_MAILIN_DIRECTORY=$HOME/codestream-mail/inbound/web/new
