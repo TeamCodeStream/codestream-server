659563ecdf26c9e41e093637c6db772e361ff365
James Price <jimmyjayp@gmail.com>
2018-01-18 23:53:35 -0500

    Configuration for API sandbox to be used to test the CI Email service

---
+++ api_server/sandbox/ci-mailin.sh
@@ -0,0 +1,10 @@
+
+. $CS_API_TOP/sandbox/defaults.sh
+
+export CS_API_MONGO_PORT=$MDB_PORT
+export CS_API_SSL_KEYFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-key
+export CS_API_SSL_CERTFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-crt
+export CS_API_SSL_CAFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-ca
+export CS_API_PORT=12179
+export CS_API_EMAIL_TO=on
+export CS_API_REPLY_TO_DOMAIN=pd.codestream.com
