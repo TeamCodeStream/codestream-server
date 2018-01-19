be39e616e44d6820c25e8767a0112ad48aa30992
Colin Stryker <colin@codestream.com>
2018-01-19 15:03:44 -0800

    add html escape utility

---
+++ shared/server_utils/html_escape.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const ENTITY_MAP = {
+ 	'&': '&amp;',
+ 	'<': '&lt;',
+ 	'>': '&gt;',
+ 	'"': '&quot;',
+ 	"'": '&#39;',
+ 	'/': '&#x2F;',
+ 	'`': '&#x60;',
+ 	'=': '&#x3D;'
+};
+
+module.exports = {
+	
+	escapeHtml: function(text) {
+		return String(text).replace(/[&<>"'`=\/]/g, function (s) {
+			return ENTITY_MAP[s];
+		});
+	}
+
+};
+
