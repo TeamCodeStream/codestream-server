b62b027b3f7c7fb1fabedced7bf9049ec35d4086
Colin Stryker <colin@codestream.com>
2018-01-24 14:38:32 -0800

    add keepAlive to pubnub initialization

--- api_server/config/pubnub.js
+++ api_server/config/pubnub.js
@@ -6,5 +6,6 @@ module.exports = {
 	publishKey: process.env.CS_API_PUBNUB_PUBLISH_KEY,
 	subscribeKey: process.env.CS_API_PUBNUB_SUBSCRIBE_KEY,
 	secretKey: process.env.CS_API_PUBNUB_SECRET,
-	ssl: true
+	ssl: true,
+	keepAlive: true
 };
