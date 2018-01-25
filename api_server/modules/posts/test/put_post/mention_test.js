862a45373bf999206d11593a7112097d7627067a
Colin Stryker <colin@codestream.com>
2018-01-25 13:47:18 -0800

    mentionedUserIds sent with post

---
+++ api_server/modules/posts/test/put_post/mention_test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var PutPostTest = require('./put_post_test');
+
+class MentionTest extends PutPostTest {
+
+	constructor (options) {
+		super(options);
+		this.wantMention = true;
+	}
+
+	get description () {
+		return 'should return the updated post with mentioned user array when updating a post with a mentioned user';
+	}
+}
+
+module.exports = MentionTest;
