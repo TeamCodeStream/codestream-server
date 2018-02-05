6ec3a2fbab0abb29052b8a9e7f9b6f45b09a96a8
James Price <jimmyjayp@gmail.com>
2018-02-05 15:01:34 -0500

    Deploying secrets for non-dev environments outside of github

--- api_server/sandbox/qa.sh
+++ api_server/sandbox/qa.sh
@@ -4,12 +4,13 @@ MONGO_ACCESS_FILE="$HOME/.codestream/mongo/qa-codestream-dbowner"
 
 . $CS_API_TOP/sandbox/defaults.sh
 
+OTHER_SECRETS_FILE=$HOME/.codestream/codestream-service/qa-api
+[ ! -f $OTHER_SECRETS_FILE ] && echo "secrets file ($OTHER_SECRETS_FILE) not found" && return 1
+. $OTHER_SECRETS_FILE
+export CS_API_AUTH_SECRET="$AUTH_SECRET"
+export CS_API_INBOUND_EMAIL_SECRET="$INBOUND_EMAIL_SECRET"
+
 unset CS_API_SETUP_MONGO
 export CS_API_PORT=8443
 unset CS_API_LOG_CONSOLE_OK
 export CS_API_REPLY_TO_DOMAIN=qa.codestream.com
-
-# edit for production
-# export CS_API_SSL_KEYFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-key
-# export CS_API_SSL_CERTFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-crt
-# export CS_API_SSL_CAFILE=/etc/pki/wildcard.codestream.us/wildcard.codestream.us-ca
