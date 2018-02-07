ea4454bf8761b71d100094884654bdcf9cf8a40d
James Price <jimmyjayp@gmail.com>
2018-02-06 19:20:51 -0500

    Config file for production

---
+++ inbound_email/sandbox/production.sh
@@ -0,0 +1,15 @@
+
+
+PUBNUB_KEYSET=$HOME/.codestream/pubnub/CodeStream-Production-Prod_Keyset
+
+. $CS_MAILIN_TOP/sandbox/defaults.sh
+
+export CS_MAILIN_REPLY_TO_DOMAIN=prod.codestream.com
+export CS_MAILIN_API_HOST=api.codestream.us
+export CS_MAILIN_API_PORT=443
+export CS_MAILIN_DIRECTORY=$HOME/codestream-mail/inbound/web/new
+
+OTHER_SECRETS_FILE=$HOME/.codestream/codestream-services/prod-mailin
+[ ! -f $OTHER_SECRETS_FILE ] && echo "secrets file ($OTHER_SECRETS_FILE) not found"
+. $OTHER_SECRETS_FILE
+export CS_API_INBOUND_EMAIL_SECRET="$INBOUND_EMAIL_SECRET"
