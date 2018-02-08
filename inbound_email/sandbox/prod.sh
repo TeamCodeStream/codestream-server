b66d001632c43565c495ae7f6dac11e4ba0c5371
James Price <jimmyjayp@gmail.com>
2018-02-08 03:33:56 -0500

    Fixing incorrect domain for api server for production

--- inbound_email/sandbox/prod.sh
+++ inbound_email/sandbox/prod.sh
@@ -5,7 +5,7 @@ PUBNUB_KEYSET=$HOME/.codestream/pubnub/CodeStream-Production-Prod_Keyset
 . $CS_MAILIN_TOP/sandbox/defaults.sh
 
 export CS_MAILIN_REPLY_TO_DOMAIN=prod.codestream.com
-export CS_MAILIN_API_HOST=api.codestream.us
+export CS_MAILIN_API_HOST=api.codestream.com
 export CS_MAILIN_API_PORT=443
 export CS_MAILIN_DIRECTORY=$HOME/codestream-mail/inbound/web/new
 
