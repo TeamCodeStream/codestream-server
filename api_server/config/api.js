435f176abbb08daa41424f3d829b3ceb935c3b16
Colin Stryker <colin@codestream.com>
2018-02-12 21:32:41 -0800

    more memorable test code

--- api_server/config/api.js
+++ api_server/config/api.js
@@ -4,5 +4,5 @@
 
 module.exports = {
 	confirmationNotRequired: process.env.CS_API_CONFIRMATION_NOT_REQUIRED,	 // avoid the email configuration by setting this env var
-	testBetaCode: 'A91C9Z'	// special beta code we use for testing
+	testBetaCode: 'C5R0CK'	// special beta code we use for testing
 };
