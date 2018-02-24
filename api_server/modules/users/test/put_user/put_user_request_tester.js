59f924645c343c36e02ffca4929cd12d81406713
Colin Stryker <colin@codestream.com>
2018-02-23 18:21:08 -0800

    support user setting timezone

--- api_server/modules/users/test/put_user/put_user_request_tester.js
+++ api_server/modules/users/test/put_user/put_user_request_tester.js
@@ -12,28 +12,28 @@ var UsernameNotUniqueTest = require('./username_not_unique_test');
 var UsernameNotUniqueForSecondTeamTest = require('./username_not_unique_for_second_team_test');
 
 const UserAttributes = require('../../user_attributes');
-const CAN_UPDATE_ATTRIBUTES = ['username', 'firstName', 'lastName'];
+const CAN_UPDATE_ATTRIBUTES = ['username', 'firstName', 'lastName', 'timeZone'];
 
 /* jshint -W071 */
 
 class PutUserRequestTester {
 
 	putUserTest () {
-                new PutUserTest().test();
+		new PutUserTest().test();
 		new UpdateMeTest().test();
 		CAN_UPDATE_ATTRIBUTES.forEach(attribute => {
 			new PutUserTest({ attributes: [attribute] }).test();
 		});
-                new ACLTest().test();
-                new ACLTeamTest().test();
-                new MessageToTeamTest().test();
-                Object.keys(UserAttributes).forEach(attribute => {
-                	if (!CAN_UPDATE_ATTRIBUTES.includes(attribute)) {
-                		new NoUpdateOtherAttributeTest({ otherAttribute: attribute }).test();
-                	}
-                });
-                new UsernameNotUniqueTest().test();
-                new UsernameNotUniqueForSecondTeamTest().test();
+		new ACLTest().test();
+		new ACLTeamTest().test();
+		new MessageToTeamTest().test();
+		Object.keys(UserAttributes).forEach(attribute => {
+			if (!CAN_UPDATE_ATTRIBUTES.includes(attribute)) {
+				new NoUpdateOtherAttributeTest({ otherAttribute: attribute }).test();
+			}
+		});
+		new UsernameNotUniqueTest().test();
+		new UsernameNotUniqueForSecondTeamTest().test();
 	}
 }
 
