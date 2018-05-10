ff5217fbf9093c032a6724947a1d9c76e4df47cd
Colin Stryker <colin@codestream.com>
2018-05-10 14:03:06 -0700

    Revert "prettiness"
    This reverts commit 090b98f58447122088b0dd96321c672b793d2bad.

--- api_server/modules/cors/cors.js
+++ api_server/modules/cors/cors.js
@@ -7,10 +7,10 @@ const CORS = require('cors');
 const URL = require('url');
 
 const _WHITELISTED_DOMAINS = [ 
-	'undefined',    // 'undefined' and 'null' can come from our plugins, not sure why (and yes, they are strings)
-	'null',
-	'codestream.com',
-	'codestream.us'
+    'undefined',    // 'undefined' and 'null' can come from our plugins, not sure why (and yes, they are strings)
+    'null',
+    'codestream.com',
+    'codestream.us'
 ];
 
 // parse domain out of url
@@ -25,59 +25,59 @@ const _parseDomain = url => {
 	if (!parsed || !parsed.hostname) {
 		return url;
 	}
-	const parts = parsed.hostname.split('.').reverse();
-	if (parts.length > 1) {
-		return `${parts[1]}.${parts[0]}`.toLowerCase();
-	}
-	else {
-		return (parts[0] || url).toLowerCase();
-	}
+    const parts = parsed.hostname.split('.').reverse();
+    if (parts.length > 1) {
+        return `${parts[1]}.${parts[0]}`.toLowerCase();
+    }
+    else {
+        return (parts[0] || url).toLowerCase();
+    }
 };
 
 class CorsModule extends APIServerModule {
 
-	getServiceOrigins () {
-		return [
-			this.api.config.teams.botOrigin    // origin for MS Teams bot
-		];
-	}
+    getServiceOrigins () {
+        return [
+            this.api.config.teams.botOrigin    // origin for MS Teams bot
+        ];
+    }
 
 	middlewares () {
-		// return a middleware function to handle CORS considerations
-		const corsOptions = {
-			origin: (origin, callback) => {
-				// no origin required if not from a browser
-				if (!origin) {
-					return callback(null, true);
-				}
+        // return a middleware function to handle CORS considerations
+        const corsOptions = {
+            origin: (origin, callback) => {
+                // no origin required if not from a browser
+                if (!origin) {
+                    return callback(null, true);
+                }
 
-				// check against whitelisted domains
-				const originDomain = _parseDomain(origin).toLowerCase();
-				if (_WHITELISTED_DOMAINS.includes(originDomain)) {
-					return callback(null, true);
-				}
+                // check against whitelisted domains
+                const originDomain = _parseDomain(origin).toLowerCase();
+                if (_WHITELISTED_DOMAINS.includes(originDomain)) {
+                    return callback(null, true);
+                }
 
-				// allow locahost in dev environments
-				if (this.api.isDevEnvironment() && originDomain === 'localhost') {
-					return callback(null, true);
-				}
+                // allow locahost in dev environments
+                if (this.api.isDevEnvironment() && originDomain === 'localhost') {
+                    return callback(null, true);
+                }
                 
-				// allow other codestream services to have their own origins
-				const serviceOrigins = this.getServiceOrigins();
-				serviceOrigins.forEach(serviceOrigin => {
-					const serviceOriginDomain = _parseDomain(serviceOrigin).toLowerCase();
-					if (originDomain === serviceOriginDomain) {
-						return callback(null, true);
-					}
-				});
+                // allow other codestream services to have their own origins
+                const serviceOrigins = this.getServiceOrigins();
+                serviceOrigins.forEach(serviceOrigin => {
+                    const serviceOriginDomain = _parseDomain(serviceOrigin).toLowerCase();
+                    if (originDomain === serviceOriginDomain) {
+                        return callback(null, true);
+                    }
+                });
 
-				const error = `unrecognized origin ${origin}`;
-				this.api.warn(error);
-				return callback(new Error(error));
-			}
-		};
-		return CORS(corsOptions);
-	}
+                const error = `unrecognized origin ${origin}`;
+                this.api.warn(error);
+                return callback(new Error(error));
+            }
+        };
+        return CORS(corsOptions);
+    }
 }
 
 module.exports = CorsModule;
