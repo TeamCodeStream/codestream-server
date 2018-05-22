afcf4a424c11ed9c51bef2c5852cc94ec9a44ca5
James Price <jimmyjayp@gmail.com>
2018-05-22 16:27:55 -0400

    tell the linter to ignore the console calls

--- api_server/bin/cs_set_reporting_group.js
+++ api_server/bin/cs_set_reporting_group.js
@@ -5,7 +5,7 @@
 // set, update or delete the reportingGroup attribute of
 // a team
 
-const logger = new console.Console(process.stdout, process.stderr);
+/* eslint no-console: 0 */
 const ConfigDirectory = process.env.CS_API_TOP + '/config';
 const MongoConfig = require(ConfigDirectory + '/mongo.js');
 const mongodb = require('mongodb');
@@ -21,45 +21,45 @@ Commander
 
 if ((!Commander.team && !Commander.teamId) ||
     (Commander.team && Commander.teamId)) {
-	logger.log('Only one value, Team or Team ID is required');
+	console.log('Only one value, Team or Team ID is required');
 	Commander.outputHelp();
 	process.exit(1);
 }
 
 if ((!Commander.group && !Commander.report) ||
     (Commander.group && Commander.report)) {
-	logger.log('Only one value, Group or Report is required');
+	console.log('Only one value, Group or Report is required');
 	Commander.outputHelp();
 	process.exit(1);
 }
 
 if (Commander.dryrun) {
-	logger.log('dry run mode - I won\'t actually do anything to the db');
+	console.log('dry run mode - I won\'t actually do anything to the db');
 }
 
 const queryCollection = async function(csDb, collection, query) {
-	// logger.log('finding', collection, query);
+	// console.log('finding', collection, query);
 	let teams;
 	try {
 		teams = await csDb.collection(collection).find(query).toArray();
 	}
 	catch(error) {
-		logger.log('error', error);
+		console.log('error', error);
 		process.exit(1);
 	}
-	// logger.log('done', teams);
+	// console.log('done', teams);
 	return teams;
 };
 
 let db;
 (async function() {
 	// connect to mongo
-	// logger.log('connecting...');
+	// console.log('connecting...');
 	try {
 		db = await MongoClient.connect(MongoConfig.url);
 	}
 	catch (error) {
-		logger.log('mongo connect error', error);
+		console.log('mongo connect error', error);
 		process.exit(1);
 	}
 	let csDb = db.db(MongoConfig.database);
@@ -72,13 +72,13 @@ let db;
 
 	// no teams found
 	if(teams.length === 0) {
-		logger.log('no matching teams found');
+		console.log('no matching teams found');
 		process.exit(1);
 	}
 
 	// multiple teams found - give useful feedback
 	if(teams.length > 1 || Commander.report) {
-		logger.log('multiple teams found');
+		console.log('multiple teams found');
 		for (let teamIdx in teams) {
 			let thisTeam = teams[teamIdx];
 
@@ -97,9 +97,9 @@ let db;
 				reportingGroup: thisTeam.reportingGroup,
 				Members: membersToDisplay
 			};
-			logger.log('Team', teamIdx, ':', teamDisplayObject, '\n');
+			console.log('Team', teamIdx, ':', teamDisplayObject, '\n');
 		}
-		logger.log('select a unique TeamId and rerun the script with -i');
+		console.log('select a unique TeamId and rerun the script with -i');
 		process.exit(1);
 	}
 
@@ -107,7 +107,7 @@ let db;
 	let operation = Commander.group === 'null' ?
 		{ $unset: { reportingGroup: '' } }:
 		{ $set: { reportingGroup: Commander.group.toLowerCase() } };
-	logger.log('updating team', teams[0].name, 'with ID', teams[0]._id, 'using', operation);
+	console.log('updating team', teams[0].name, 'with ID', teams[0]._id, 'using', operation);
 	if (Commander.dryrun) {
 		process.exit(0);
 	}
@@ -117,7 +117,7 @@ let db;
 			operation);
 	}
 	catch(error) {
-		logger.log('update error', error);
+		console.log('update error', error);
 		process.exit(1);
 	}
 	process.exit(0);
