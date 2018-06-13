50bc89ea10e4e90bfc6cfeefe2e3699a09153a0a
Colin Stryker <colin@codestream.com>
2018-06-13 14:56:23 -0700

    comma

--- api_server/config/api.js
+++ api_server/config/api.js
@@ -8,6 +8,6 @@ module.exports = {
 	unauthenticatedPaths: ['^\\/no-auth\\/'],	// matching these paths means Authorization header is not required
 	optionalAuthenticatedPaths: ['^\\/help(\\/|$)'],	// matching these paths means Authorization header is optional, behavior may vary
 	helpIsAvailable: process.env.CS_API_HELP_AVAILABLE,	// if this is set, API server /help is available
-	forgotPasswordExpiration: process.env.CS_API_FORGOT_PASSWORD_EXPIRATION || 24 * 60 * 60 * 1000	// how long a token for forgot-password remains valid
+	forgotPasswordExpiration: process.env.CS_API_FORGOT_PASSWORD_EXPIRATION || 24 * 60 * 60 * 1000,	// how long a token for forgot-password remains valid
 	confirmationExpiration: process.env.CS_API_CONFIRMATION_EXPIRATION || 24 * 60 * 60 * 1000	// how long a token for email confirmation remains valid
 };
