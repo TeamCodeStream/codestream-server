1ce61d3d8564a4c817c23c0dc7f98d3f4f0467e4
Colin Stryker <colin@codestream.com>
2018-08-20 13:41:04 -0700

    supplemental data to go with posts in pubnub messages even for stream channels

--- api_server/modules/posts/post_publisher.js
+++ api_server/modules/posts/post_publisher.js
@@ -13,11 +13,13 @@ class PostPublisher {
 		if (this.stream.type === 'file' || this.stream.isTeamStream) {
 			// for file-type streams, or team-streams (streams for which everyone on the team is a member),
 			// we publish to the team that owns the stream
-			await this.publishPostToTeam();
+			const channel = `team-${this.stream.teamId}`;
+			await this.publishPostToChannel(channel);
 		}
 		else {
 			// for channels and direct, we publish to the stream itself
-			await this.publishPostToStream();
+			const channel = `stream-${this.stream._id}`;
+			await this.publishPostToChannel(channel);
 
 			// since the post went to a private stream, if there are any repos affected,
 			// we need to publish that info to the whole team
@@ -25,10 +27,8 @@ class PostPublisher {
 		}
 	}
 
-	// publish a post to a team channel
-	async publishPostToTeam () {
-		const teamId = this.stream.teamId;
-		const channel = 'team-' + teamId;
+	// publish a post to the given channel
+	async publishPostToChannel (channel) {
 		const message = {
 			post: this.data.post,
 			requestId: this.request.request.id
@@ -47,28 +47,7 @@ class PostPublisher {
 		}
 		catch (error) {
 			// this doesn't break the chain, but it is unfortunate...
-			this.request.warn(`Could not publish post message to team ${teamId}: ${JSON.stringify(error)}`);
-		}
-	}
-
-	// publish a post to a stream channel
-	async publishPostToStream () {
-		const streamId = this.stream._id;
-		const channel = 'stream-' + streamId;
-		const message = {
-			post: this.data.post,
-			requestId: this.request.request.id
-		};
-		try {
-			await this.messager.publish(
-				message,
-				channel,
-				{ request: this.request }
-			);
-		}
-		catch (error) {
-			// this doesn't break the chain, but it is unfortunate...
-			this.request.warn(`Could not publish post message to stream ${streamId}: ${JSON.stringify(error)}`);
+			this.request.warn(`Could not publish post message to channel ${channel}: ${JSON.stringify(error)}`);
 		}
 	}
 
