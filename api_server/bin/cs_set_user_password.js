7ae4fd9be75f092e341f641fde07ac34517da636
Colin Stryker <colin@codestream.com>
2018-08-24 14:09:40 -0700

    script to set a user password

---
+++ api_server/bin/cs_set_user_password.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+// utility script to set a user's password
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const PasswordHasher = require(process.env.CS_API_TOP + '/modules/users/password_hasher');
+const Email = process.argv[2];
+const Password = process.argv[3];
+const MongoClient = require(process.env.CS_API_TOP + '/lib/util/mongo/mongo_client');
+const MongoConfig = require(process.env.CS_API_TOP + '/config/mongo');
+const UserIndexes = require(process.env.CS_API_TOP + '/modules/users/indexes');
+
+if (!Email || !Password) {
+	console.log('Usage: cs_set_user_password <email> <password>');
+	process.exit();
+}
+
+(async function() {
+
+	const mongoClient = new MongoClient();
+	const mongoConfig = Object.assign({}, MongoConfig, { collections: ['users'] });
+	delete mongoConfig.queryLogging;
+	try {
+		await mongoClient.openMongoClient(mongoConfig);
+	}
+	catch (error) {
+		console.error(`unable to open mongo client: ${JSON.stringify(error)}`);
+		process.exit();
+	}
+	const userCollection = mongoClient.mongoCollections.users;
+
+	const hash = await new PasswordHasher({ password: Password }).hashPassword();
+
+	try {
+		const users = await userCollection.getByQuery(
+			{ searchableEmail: Email.toLowerCase() },
+			{ hint: UserIndexes.bySearchableEmail }
+		);
+		if (users.length === 0) {
+			console.error('User not found: ' + Email);
+			process.exit();
+		}
+		await mongoClient.mongoCollections['users'].updateDirect(
+			{ searchableEmail: Email.toLowerCase() }, 
+			{ $set: { passwordHash: hash } }
+		);
+	} 
+	catch (error) {
+		console.error(`unable to set password hash for user: ${JSON.stringify(error)}`);
+		process.exit();
+	}
+
+	console.log('Password hash changed');
+	process.exit();
+})();
+
