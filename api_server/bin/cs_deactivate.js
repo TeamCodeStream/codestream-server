22cd2e5112a5b26b4a39861cf1655b6403000be0
Colin Stryker <colin@codestream.com>
2018-10-02 15:27:00 -0700

    script to deactivate teams

---
+++ api_server/bin/cs_deactivate.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const Commander = require('commander');
+const Deactivator = require(process.env.CS_API_TOP + '/lib/util/deactivator');
+
+Commander
+	.option('-t, --team <teamIdOrName>', 'Deactivate team')
+	.option('-r, --repo <repoId>', 'Deactivate repo')
+	.option('-u, --user <userIdOrEmail>', 'Deactivate user')
+	.option('--dtu, --deactivate-teamless-users', 'Deactivate teamless users when done')
+	.parse(process.argv);
+
+if (!Commander.team && !Commander.repo && !Commander.user) {
+	console.warn('team, repo, or user required');
+	process.exit();
+}
+
+let options = {
+	teamIdOrName: Commander.team,
+	repoId: Commander.repo,
+	userIdOrEmail: Commander.user,
+	deactivateTeamlessUsers: Commander.deactivateTeamlessUsers
+};
+
+(async function() {
+	try {
+		await new Deactivator().go(options);
+		process.exit();
+	}
+	catch (error) {
+		console.error(error);
+		process.exit();
+	}
+})();
---
+++ api_server/lib/util/deactivator.js
@@ -0,0 +1,431 @@
+const MongoClient = require(process.env.CS_API_TOP + '/server_utils/mongo/mongo_client');
+const MongoConfig = require(process.env.CS_API_TOP + '/config/mongo');
+const ObjectID = require('mongodb').ObjectID;
+const UserIndexes = require(process.env.CS_API_TOP + '/modules/users/indexes');
+const RepoIndexes = require(process.env.CS_API_TOP + '/modules/repos/indexes');
+const StreamIndexes = require(process.env.CS_API_TOP + '/modules/streams/indexes');
+
+const COLLECTIONS = ['companies', 'teams', 'repos', 'users', 'streams', 'posts', 'markers', 'markerLocations'];
+
+
+class Deleter {
+
+	async go (options) {
+		Object.assign(this, options);
+		this.logger = this.logger || console;
+		await this.openMongoClient();
+		await this.getTargetObject();
+		await this.getUsers();
+		await this.getRepos();
+		await this.getStreams();
+		await this.deleteRepos();
+		await this.deleteTeam();
+		await this.deleteCompany();
+		await this.deleteUsers();
+		await this.deleteStreams();
+		await this.deletePosts();
+		await this.deleteMarkers();
+		await this.deleteMarkerLocations();
+	}
+
+	async openMongoClient () {
+		this.mongoClient = new MongoClient();
+		let mongoConfig = Object.assign({}, MongoConfig, { collections: COLLECTIONS });
+		delete mongoConfig.queryLogging;
+		try {
+			await this.mongoClient.openMongoClient(mongoConfig);
+		}
+		catch (error) {
+			throw `unable to open mongo client: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async getTargetObject () {
+		if (this.teamIdOrName) {
+			await this.getTeam();
+		}
+		else if (this.repoId) {
+			await this.getRepo();
+		}
+		else if (this.userIdOrEmail) {
+			await this.getUser();
+		}
+		else {
+			throw 'must provide team, repo, or user';
+		}
+	}
+
+	async getTeam () {
+		if (!this.teamIdOrName) {
+			return;
+		}
+		try {
+			this.teamId = ObjectID(this.teamIdOrName).toString();
+			await this.getTeamById();
+		}
+		catch (error) {
+			await this.getTeamByName();
+		}
+	}
+
+	async getTeamById () {
+		try {
+			this.team = await this.mongoClient.mongoCollections.teams.getById(
+				this.teamId,
+				{ fields: ['_id', 'companyId'] }
+			);
+		}
+		catch (error) {
+			throw `unable to fetch team: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async getTeamByName () {
+		let teams;
+		try {
+			teams = await this.mongoClient.mongoCollections.teams.getByQuery(
+				{
+					name: this.teamIdOrName
+				},
+				{
+					fields: ['_id', 'companyId'],
+					overrideHintRequired: true
+				}
+			);
+		}
+		catch (error) {
+			throw `unable to fetch team: ${JSON.stringify(error)}`;
+		}
+		if (teams.length === 0) {
+			throw `team not found: ${this.teamIdOrName}`;
+		}
+		this.team = teams[0];
+		this.teamId = this.team._id;
+	}
+
+	async getRepo () {
+		try {
+			this.repo = this.mongoClient.mongoCollections.repos.getById(this.repoId);
+		}
+		catch (error) {
+			throw `unable to fetch repo: ${JSON.stringify(error)}`;
+		}
+		if (!this.repo) {
+			throw `repo not found: ${this.repoId}`;
+		}
+	}
+
+	async getUser () {
+		try {
+			this.userId = ObjectID(this.userIdOrEmail).toString();
+			await this.getUserById();
+		}
+		catch (error) {
+			await this.getUserByEmail();
+		}
+	}
+
+	async getUserByEmail () {
+		let users;
+		try {
+			users = await this.mongoClient.mongoCollections.users.getByQuery(
+				{
+					searchableEmail: this.userIdOrEmail.toLowerCase()
+				},
+				{
+					fields: ['_id'],
+					hint: UserIndexes.bySearchableEmail
+				}
+			);
+		}
+		catch (error) {
+			throw `unable to fetch user by email: ${JSON.stringify(error)}`;
+		}
+		if (users.length === 0) {
+			throw `user not found: ${this.userIdOrEmail}`;
+		}
+		this.user = users[0];
+	}
+
+	async getUserById () {
+		try {
+			this.user = await this.mongoClient.mongoCollections.users.getById(this.userIdOrEmail);
+		}
+		catch (error) {
+			throw `unable to fetch user: ${JSON.stringify(error)}`;
+		}
+		if (!this.user) {
+			throw `user not found: ${this.userIdOrEmail}`;
+		}
+	}
+
+	async getUsers () {
+		if (!this.teamId) {
+			return;
+		}
+		try {
+			this.users = await this.mongoClient.mongoCollections.users.getByQuery(
+				{
+					teamIds: this.teamId
+				},
+				{
+					fields: ['_id', 'teamIds'],
+					hint: UserIndexes.byTeamIds
+				}
+			);
+		}
+		catch (error) {
+			throw `unable to fetch users by team: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async getRepos () {
+		if (!this.teamId) {
+			return;
+		}
+		let repos;
+		try {
+			repos = await this.mongoClient.mongoCollections.repos.getByQuery(
+				{
+					teamId: this.teamId
+				},
+				{
+					fields: ['_id'],
+					hint: RepoIndexes.byTeamId
+				}
+			);
+		}
+		catch (error) {
+			throw `unable to fetch repos by team: ${JSON.stringify(error)}`;
+		}
+		this.repoIds = repos.map(repo => repo._id);
+	}
+
+	async getStreams () {
+		let query, hint;
+		if (this.teamId) {
+			query = { teamId: this.teamId };
+			hint = StreamIndexes.byName;
+		}
+		else {
+			if (!this.repoIds && !this.repoId) {
+				return;
+			}
+			this.repoIds = this.repoIds || [this.repoId];
+			query = { repoId: { $in: this.repoIds } };
+			hint = StreamIndexes.byFile;
+		}
+		let streams;
+		try {
+			streams = await this.mongoClient.mongoCollections.streams.getByQuery(
+				query,
+				{
+					fields: ['_id'],
+					hint: hint
+				}
+			);
+		}
+		catch (error) {
+			throw `unable to fetch streams by repo: ${JSON.stringify(error)}`;
+		}
+		this.streamIds = streams.map(stream => stream._id);
+	}
+
+	async deleteRepos () {
+		if (this.teamId) {
+			await this.deleteReposByTeamId();
+		}
+		else if (this.repoId) {
+			await this.deleteRepoById();
+		}
+	}
+
+	async deleteReposByTeamId () {
+		this.logger.log(`Deactivating repos in team ${this.teamId}...`);
+		try {
+			await this.mongoClient.mongoCollections.repos.updateDirect(
+				{ teamId: this.teamId },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete repo: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteRepoById () {
+		const repoId = this.repoId.toLowerCase();
+		this.logger.log(`Deactivating repo ${repoId}...`);
+		try {
+			await this.mongoClient.mongoCollections.repos.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.repos.objectIdSafe(repoId) },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete repo: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteTeam () {
+		if (!this.teamId) {
+			return ;
+		}
+		const teamId = this.teamId.toLowerCase();
+		this.logger.log(`Deactivating team ${teamId}...`);
+		try {
+			await this.mongoClient.mongoCollections.teams.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.teams.objectIdSafe(teamId) },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete team: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteCompany () {
+		if (!this.team) {
+			return;
+		}
+		this.logger.log(`Deactivating company ${this.team.companyId}...`);
+		try {
+			await this.mongoClient.mongoCollections.companies.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.companies.objectIdSafe(this.team.companyId) },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete company: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteUsers () {
+		if (this.teamId) {
+			await this.removeUsersFromTeam();
+			if (this.deleteTeamlessUsers) {
+				await this.deleteWouldBeTeamlessUsers();
+			}
+		}
+		else if (this.userIdOrEmail) {
+			await this.deleteSingleUser();
+		}
+	}
+
+	async deleteWouldBeTeamlessUsers () {
+		const userIdsToDelete = this.users
+			.filter(user => {
+				return (
+					(user.teamIds || []).length === 1 &&
+					user.teamIds[0] === this.teamId
+				);
+			})
+			.map(user => user._id);
+
+		if (userIdsToDelete.length === 0) {
+			return;
+		}
+		this.logger.log(`Deactivating ${userIdsToDelete.length} users...`);
+		try {
+			await this.mongoClient.mongoCollections.users.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.users.inQuerySafe(userIdsToDelete) },
+				{ $set: { deactivated: true, searchableEmail: 'deactivated' + Date.now() } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete users: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async removeUsersFromTeam () {
+		this.logger.log('Removing users from team...');
+		try {
+			await this.mongoClient.mongoCollections.users.updateDirect(
+				{ teamIds: this.teamId },
+				{ $pull: { teamIds: this.teamId } },
+			);
+		}
+		catch (error) {
+			throw `unable to remove users from team: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteSingleUser () {
+		this.logger.log(`Deactivating user ${this.userIdOrEmail}...`);
+		try {
+			await this.mongoClient.mongoCollections.users.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.users.objectIdSafe(this.user._id) },
+				{ $set: { deactivated: true, searchableEmail: 'deactivated' + Date.now() } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete user: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteStreams () {
+		if (!this.streamIds || this.streamIds.length === 0) {
+			return;
+		}
+		this.logger.log(`Deactivating ${this.streamIds.length} streams...`);
+		try {
+			await this.mongoClient.mongoCollections.streams.updateDirect(
+				{ _id: this.mongoClient.mongoCollections.streams.inQuerySafe(this.streamIds) },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete streams: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deletePosts () {
+		if (!this.streamIds || this.streamIds.length === 0) {
+			return ;
+		}
+		this.logger.log(`Deactivating posts in ${this.streamIds.length} streams...`);
+		try {
+			await this.mongoClient.mongoCollections.posts.updateDirect(
+				{ streamId: { $in: this.streamIds } },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete posts: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteMarkers () {
+		if (!this.streamIds || this.streamIds.length === 0) {
+			return;
+		}
+		this.logger.log(`Deactivating markers in ${this.streamIds.length} streams...`);
+		try {
+			await this.mongoClient.mongoCollections.markers.updateDirect(
+				{ streamId: { $in: this.streamIds } },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete markers: ${JSON.stringify(error)}`;
+		}
+	}
+
+	async deleteMarkerLocations () {
+		if (!this.streamIds || this.streamIds.length === 0) {
+			return ;
+		}
+		this.logger.log(`Deactivating marker locations in ${this.streamIds.length} streams...`);
+		try {
+			await this.mongoClient.mongoCollections.markerLocations.updateDirect(
+				{ streamId: { $in: this.streamIds } },
+				{ $set: { deactivated: true } }
+			);
+		}
+		catch (error) {
+			throw `unable to delete marker locations: ${JSON.stringify(error)}`;
+		}
+	}
+}
+
+module.exports = Deleter;
