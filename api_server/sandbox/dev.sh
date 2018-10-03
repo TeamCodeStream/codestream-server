48d1f522f77e70763386ff5fe28b7d8c19b038a8
James Price <jimmyjayp@gmail.com>
2018-10-02 23:22:47 -0400

    Polish and updates for dev build

--- api_server/sandbox/dev.sh
+++ api_server/sandbox/dev.sh
@@ -10,9 +10,9 @@ export CS_API_ASSET_ENV=dev
 . $CS_API_TOP/sandbox/defaults.sh
 
 # export CS_API_PORT=9443
-# export CS_API_REPLY_TO_DOMAIN=pd.codestream.com
-# export CS_API_OUTBOUND_EMAIL_SQS="pd_outboundEmail"
+export CS_API_REPLY_TO_DOMAIN=ci.codestream.com
+export CS_API_OUTBOUND_EMAIL_SQS="ci_outboundEmail"
 # export CS_API_SLACKBOT_ORIGIN=http://pd-bot.codestream.us:11079
 # export CS_API_TEAMSBOT_ORIGIN=http://pd-bot.codestream.us:10079
 # export CS_API_WEB_CLIENT_ORIGIN=http://pd-app.codestream.us:1380
-unset CS_API_SUPPRESS_EMAILS
+# unset CS_API_SUPPRESS_EMAILS
