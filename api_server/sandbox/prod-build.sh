200bb6df6f813af06f81f7c325440caef0094d62
James Price <jimmyjayp@gmail.com>
2018-10-16 21:39:34 -0400

    Trying to fix API build issues by sepearating CI vars from dev

--- api_server/sandbox/prod-build.sh
+++ api_server/sandbox/prod-build.sh
@@ -1,18 +1,25 @@
 
+# This provides an environment for building the prod asset as
+# well as running supporting API sandboxes for other builds.
+
 PUBNUB_KEY_FILE="$HOME/.codestream/pubnub/CodeStream-Development-CI_Keyset"
 #MONGO_ACCESS_FILE=
 #SSL_CERT=
 #MIXPANEL_TOKEN_FILE=
 #BOT_SECRETS_FILE=
-#OTHER_SECRETS_FILE=
+#SLACK_API_ACCESS_FILE=
+OTHER_SECRETS_FILE="$HOME/.codestream/codestream-services/ci-api"
 export CS_API_ASSET_ENV=prod
 
 . $CS_API_TOP/sandbox/defaults.sh
 
-# export CS_API_PORT=9443
+# This variable can be overridden when the sandbox is installed
+[ -z "$CS_API_PORT" ] && export CS_API_PORT=37201
 export CS_API_REPLY_TO_DOMAIN=ci.codestream.com
 export CS_API_OUTBOUND_EMAIL_SQS="ci_outboundEmail"
-# export CS_API_SLACKBOT_ORIGIN=http://pd-bot.codestream.us:11079
-# export CS_API_TEAMSBOT_ORIGIN=http://pd-bot.codestream.us:10079
-# export CS_API_WEB_CLIENT_ORIGIN=http://pd-app.codestream.us:1380
-# unset CS_API_SUPPRESS_EMAILS
+export CS_API_SENDER_EMAIL=ci-alerts@codestream.com
+
+# These are fake ports
+export CS_API_SLACKBOT_ORIGIN=http://localhost.codestream.us:37202
+export CS_API_TEAMSBOT_ORIGIN=http://localhost.codestream.us:37203
+export CS_API_WEB_CLIENT_ORIGIN=http://localhost.codestream.us:37204
