2dd41f5e8b2a7ae0f455aa4360cd28eb27d4a065
Colin Stryker <colin@codestream.com>
2018-10-17 10:32:19 -0700

    make confirmation cheat an env var

--- inbound_email/config/secrets.js
+++ inbound_email/config/secrets.js
@@ -4,5 +4,5 @@
 
 module.exports = {
 	mailSecret: process.env.CS_MAILIN_SECRET, // for internal comms with API server
-	confirmationCheat: 'H5(bt#@QkMz$'	// for bypassing email confirmation, used for unit testing
+	confirmationCheat: process.env.CS_MAILIN_CONFIRMATION_CHEAT_CODE	// for bypassing email confirmation, used for unit testing
 };
