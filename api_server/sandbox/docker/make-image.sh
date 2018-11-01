d5c7f024eb9970f74b198cf9fdb02745ecc3d530
James Price <jimmyjayp@gmail.com>
2018-11-01 17:41:22 -0400

    Adding docker step to build script

--- api_server/sandbox/docker/make-image.sh
+++ api_server/sandbox/docker/make-image.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+function usage {
+	echo "usage: $0 [-V docker-version]"
+	exit 1
+}
+
+dockerVer=""
+dockerImage=teamcodestream/local-api
 dockerDir=sandbox/docker
 edir=$dockerDir/extras
 
+while getopts "V:" arg
+do
+	case $arg in
+	V) dockerVer=":$OPTARG";;
+	*) usage
+	esac
+done
+
+dt-docker-util -a rmi --image-name $dockerImage$dockerVer || exit 1
+
 cd $CS_API_TOP
-#npm run veryclean
+npm run veryclean
 [ ! -d $edir ] && mkdir $edir
 cp $CS_API_SSL_KEYFILE $CS_API_SSL_CERTFILE $CS_API_SSL_CAFILE $edir
 
@@ -18,6 +35,6 @@ EOF
 
 (for i in `cs_api-vars | egrep -v -e '^(CS_API_MONGO_HOST|CS_API_SUPPRESS_EMAILS|CS_API_SSL_CAFILE|CS_API_SSL_CERTFILE|CS_API_SSL_KEYFILE)='| sed -e "s|$CS_API_SANDBOX|/opt/api|g"`; do echo ENV $i; done) >>$edir/buildvars.env
 
-create-file-from-template.py -o $dockerDir/Dockerfile -t $dockerDir/Dockerfile.template
+create-file-from-template.py -o $dockerDir/Dockerfile -t $dockerDir/Dockerfile.template || exit 1
 
-docker build -t teamcodestream/local-api -f sandbox/docker/Dockerfile .
+docker build -t $dockerImage$dockerVer -f $dockerDir/Dockerfile .
