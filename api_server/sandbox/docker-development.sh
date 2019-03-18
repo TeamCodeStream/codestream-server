7215271d9ed07155eaae6fb97771a882b85a0b23
James Price <jimmyjayp@gmail.com>
2019-03-18 16:24:38 -0400

    Need NODE_PATH for container development hack

--- api_server/sandbox/docker-development.sh
+++ api_server/sandbox/docker-development.sh
@@ -3,7 +3,7 @@
 # in our docker development container
 
 export CS_API_NODE_MODULES_DIR=/codestream/node_mods/$CS_API_NAME
-export CS_API_NODE_MODULES_DIR=$HOME/node_mods/$CS_API_NAME
+export NODE_PATH=$CS_API_NODE_MODULES_DIR
 . $CS_API_TOP/sandbox/defaults.sh
 
 export CS_API_NPM_INSTALL_XTRA_OPTS="--unsafe-perm --prefix=$CS_API_NODE_MODULES_DIR"
