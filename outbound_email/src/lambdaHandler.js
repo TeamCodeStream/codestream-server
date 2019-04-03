2aa6c048ac5ff090043e1e26e4d6b8c7c851a95a
Colin Stryker <colin@codestream.com>
2019-04-02 19:41:27 -0700

    stupid colin

--- outbound_email/src/lambdaHandler.js
+++ outbound_email/src/lambdaHandler.js
@@ -7,17 +7,20 @@ exports.handler = async function(event) {
 	try {
 		console.log('GOT AN EVENT: ' + JSON.stringify(event, undefined, 5));
 		const config = Object.assign({}, Config, { dontListen: true });
-		OutboundEmailService = OutboundEmailService || new OutboundEmailServer(config);
-		await OutboundEmailService.start(async error => {
-			console.log('SERVICE STARTED');
-			if (error) {
+		if (!OutboundEmailService) {
+			OutboundEmailService = new OutboundEmailServer(config);
+			try {
+				await OutboundEmailService.start();
+			}
+			catch (error) {
 				console.error('server failed to start: ' + error); // eslint-disable-line no-console
 				process.exit();	
 			}
-			console.log('CALLING lambda');
-			await OutboundEmailService.lambda(event);
-			console.log('CALLED lambda');
-		});
+		}
+		console.log('SERVICE STARTED');
+		console.log('CALLING lambda');
+		await OutboundEmailService.lambda(event);
+		console.log('CALLED lambda');
 	}
 	catch (error) {
 		console.warn('Error processing lambda event:', error);
