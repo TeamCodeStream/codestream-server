45548cadb6293f85ba73d816e3cbe09a5abc9fff
James Price <jimmyjayp@gmail.com>
2019-04-15 21:24:04 -0400

    Adding socketcluster to unified config

--- api_server/config/socketcluster.js
+++ api_server/config/socketcluster.js
@@ -2,8 +2,21 @@
 
 'use strict';
 
-module.exports = {
-	host: process.env.CS_API_SOCKETCLUSTER_HOST,
-	port: process.env.CS_API_SOCKETCLUSTER_PORT,
-	dontRejectUnauthorized: process.env.CS_API_SOCKETCLUSTER_DONT_REJECT_UNAUTHORIZED
-};
+let ClusterCfg = {};
+if (process.env.CS_API_CFG_FILE) {
+	let Cfg = require(process.env.CS_API_CFG_FILE);
+	ClusterCfg = {
+		host: Cfg.broadcastEngine.codestreamBroadcaster.host,
+		port: Cfg.broadcastEngine.codestreamBroadcaster.port,
+		dontRejectUnauthorized: true
+	};
+}
+else {
+	ClusterCfg = {
+		host: process.env.CS_API_SOCKETCLUSTER_HOST,
+		port: process.env.CS_API_SOCKETCLUSTER_PORT,
+		dontRejectUnauthorized: process.env.CS_API_SOCKETCLUSTER_DONT_REJECT_UNAUTHORIZED
+	};
+}
+
+module.exports = ClusterCfg;
