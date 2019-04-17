cbd9b9535736fa0f068b455e6ffef42738f237f5
Colin Stryker <colin@codestream.com>
2019-04-17 15:57:58 -0700

    real test account client ID for youtrack

--- api_server/modules/youtrack_auth/youtrack_auth.js
+++ api_server/modules/youtrack_auth/youtrack_auth.js
@@ -6,8 +6,6 @@ const OAuth2Module = require(process.env.CS_API_TOP + '/lib/oauth2/oauth2_module
 
 const OAUTH_CONFIG = {
 	provider: 'youtrack',
-	host: 'colincodestream.myjetbrains.com/youtrack',
-	apiHost: 'colincodestream.myjetbrains.com/youtrack/api/rest',
 	authPath: 'api/rest/oauth2/auth',
 	scopes: 'YouTrack',
 	additionalAuthCodeParameters: {
@@ -25,22 +23,6 @@ class YouTrackAuth extends OAuth2Module {
 		super(config);
 		this.oauthConfig = OAUTH_CONFIG;
 	}
-
-	/*
-	// get redirect parameters and url to use in the redirect response
-	// here we override the usual method to deal with some trello peculiarities
-	getRedirectData (options) {
-		const data = super.getRedirectData(options);
-		const { state, redirectUri } = options;
-		data.parameters.return_url = `${redirectUri}?state=${state}`;
-		delete data.parameters.redirect_uri;
-		const { apiKey } = this.api.config.trello;
-		data.parameters.key = apiKey;
-		delete data.parameters.client_id;
-		delete data.parameters.state;
-		return data;
-	}
-	*/
 }
 
 module.exports = YouTrackAuth;
