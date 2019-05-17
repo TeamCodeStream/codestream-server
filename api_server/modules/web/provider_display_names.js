68edc6fe311d4c0b3fc78654668a2bdbf20a15da
Colin Stryker <colin@codestream.com>
2019-05-16 19:45:05 -0700

    add file

---
+++ api_server/modules/web/provider_display_names.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'github': 'GitHub',
+	'bitbucket': 'Bitbucket',
+	'gitlab': 'GitLab',
+	'trello': 'Trello',
+	'jira': 'Jira',
+	'asana': 'Asana',
+	'youtrack': 'YouTrack',
+	'slack': 'Slack',
+	'azuredevops': 'Azure DevOps',
+	'vsts': 'Visual Studio Team Services'
+};
+
