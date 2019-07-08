47de9f171345cc5cebbfc79299520f293e992670
Colin Stryker <colin@codestream.com>
2019-07-08 15:41:32 -0700

    try indefinitely to connect to mongo

--- broadcaster/codestream/codestream.js
+++ broadcaster/codestream/codestream.js
@@ -1,3 +1,5 @@
+// this file injects CodeStream related functionality into the generic socket cluster server
+
 'use strict';
 
 const MongoClient = require('./server_utils/mongo/mongo_client');
@@ -17,18 +19,23 @@ class CodeStream {
 		this.connectToMongo();
 	}
 
+	// add middleware, which acts on socket cluster events
 	addMiddlware () {
 		const { scServer } = this.scWorker;
+
+		// respond to channel subscribe event
 		scServer.addMiddleware(scServer.MIDDLEWARE_SUBSCRIBE, (request, next) => {
 			this.onSubscribe(request, next);
 		});
 	}
 
+	// add listeners to a socket connection
 	addConnectionListeners () {
 		const { scServer } = this.scWorker;
 		scServer.on('connection', this.addSocketListeners.bind(this));
 	}
 
+	// whenever a channel is subscribed to...
 	async onSubscribe (request, next) {
 		const { socket, channel } = request;
 		const { authToken } = socket;
@@ -37,11 +44,13 @@ class CodeStream {
 		}
 		const { uid } = authToken;
 
+		// authorize the subscription, fail out if an error string is returned
 		const reason = await this.authorizeSubscribe(uid, channel);
 		if (reason) {
 			return next(`not authorized for channel ${channel}: ${reason}`);
 		}
 
+		// keep track of mappings between sockets, channels, and user IDs
 		this.socketsByUidAndChannel[uid] = this.socketsByUidAndChannel[uid] || {};
 		const uidSockets = this.socketsByUidAndChannel[uid];
 		uidSockets[channel] = uidSockets[channel] || [];
@@ -49,6 +58,7 @@ class CodeStream {
 
 		this.uidsBySocketId[socket.id] = uid;
 
+		// also keep track of users subscribed to team channels, which helps with presence
 		if (channel.startsWith('team-')) {
 			this.uidsByTeamChannel[channel] = this.uidsByTeamChannel[channel] || [];
 			if (!this.uidsByTeamChannel[channel].includes(uid)) {
@@ -59,10 +69,12 @@ class CodeStream {
 		next();
 	}
 
+	// authorize an attempt to subscribe to a channel
 	async authorizeSubscribe (uid, channel) {
 		if (!this.data) { return; }
 		let user;
 		try {
+			// get the user data associated with this subscription
 			user = await this.data.users.getById(uid);
 		}
 		catch (error) {
@@ -74,12 +86,14 @@ class CodeStream {
 			return 'user not found';
 		}
 
+		// authorize according to the channel type
 		const parts = channel.split('-');
 		if (parts[0] === 'stream') {
 			return await this.authorizeStream(user, parts[1]);
 		}
 
 		if (parts[0] === 'user') {
+			// users can only subscribe to their own channel
 			if (parts[1] !== uid) {
 				return 'user mismatch';
 			}
@@ -92,9 +106,11 @@ class CodeStream {
 		}
 	}
 
+	// authorize a user's attempt to subscribe to a stream channel
 	async authorizeStream (user, streamId) {
 		let stream;
 		try {
+			// get the stream
 			stream = await this.data.streams.getById(streamId);
 		}
 		catch (error) {
@@ -109,40 +125,58 @@ class CodeStream {
 				stream.isTeamStream
 			)
 		) {
+			// file-type streams and team streams are authorized per the team
 			return this.authorizeTeam(user, stream.teamId);
 		}
 		else if (!(stream.memberIds || []).includes(user._id)) {
+			// otherwise the user must be a member of the stream
 			return 'user not in stream';
 		}
 	}
 
+	// authorize the user for access to a team-related channel
 	authorizeTeam (user, teamId) {
+		// user must be in the team
 		if (!(user.teamIds || []).includes(teamId)) {
 			return 'user not in team';
 		}
 	}
 
+	// add listeners to various custom socket events
 	addSocketListeners (socket) {
+
+		// authorize a connection
 		socket.on('auth', data => {
 			this.handleAuth(socket, data);
 		});
+
+		// desubscribe a socket from a channel (initiated by the API server)
 		socket.on('desubscribe', data => {
 			this.handleDesubscribe(socket, data);
 		});
+
+		// unsubscribe from a channel (initiated by the client)
 		socket.on('unsubscribe', data => {
 			this.handleUnsubscribe(socket, data);
 		});
+
+		// get the message history for a particular socket
 		socket.on('getHistory', request => {
 			this.getHistory(socket, request);
 		});
+
+		// get the users currently subscribed to a team channel
 		socket.on('getSubscribedUsers', request => {
 			this.getSubscribedUsers(socket, request);
 		});
+
+		// handle an incoming message
 		socket.on('message', data => {
 			this.handleMessage(socket, data);
 		});
 	}
 	
+	// handle an client trying to authorize their socket connection
 	async handleAuth (socket, data) {
 		if (!data.token) {
 			return socket.emit('error', new Error('AuthError: no auth token provided'));
@@ -157,17 +191,25 @@ class CodeStream {
 			const message = error instanceof Error ? error.message : JSON.stringify(error);
 			return socket.emit('error', new Error(`AuthError: ${message}`));
 		}
+
+		// all good, set the token for the socket and emit an 'authed' event letting the
+		// client know they are actually authed
 		socket.setAuthToken({ uid: data.uid });
 		socket.emit('authed');
 	}
 
+	// validate a token associated with a socket connection
 	async validateToken (token, uid, subscriptionCheat) {
 		if (!this.data) { throw 'not connected to db'; }
+
+		// API connections are special and require a secret
 		if (uid === 'API' && token === Config.secrets.api) {
 			return;
 		}
+
 		let user;
 		try {
+			// get the user associated with the token
 			user = await this.data.users.getById(uid);
 		}
 		catch (error) {
@@ -178,7 +220,10 @@ class CodeStream {
 		if (!user || user.deactivated) {
 			throw 'user not found';
 		}
+
+		// verify the provided token matches their stored token
 		if (token !== user.broadcasterToken) {
+			// special condition for integration tests
 			if (subscriptionCheat === Config.secrets.subscriptionCheat) {
 				this.warn('NOTE: Allowing subscription with user ID, this had better be a test!!!');
 			}
@@ -188,10 +233,11 @@ class CodeStream {
 		}
 	}
 
+	// connect to the mongo database
 	async connectToMongo () {
-		this.mongoClient = new MongoClient();
+		this.mongoClient = new MongoClient({ tryIndefinitely: true });
 		const collections = ['users', 'streams', 'messages'];
-		let mongoConfig = Object.assign({}, Config.mongo, { collections });
+		let mongoConfig = Object.assign({}, Config.mongo, { collections, logger: this });
 		try {
 			await this.mongoClient.openMongoClient(mongoConfig);
 			this.data = this.mongoClient.mongoCollections;
@@ -204,15 +250,21 @@ class CodeStream {
 		return this.mongoClient;
 	}
 
+	// handle a de-subscribe request from the server ... this is a forced de-subscribe 
+	// for the sockets associated with a user to no longer be subscribed to a given channel
 	handleDesubscribe (socket, data) {
+		// this request is only allowed from the server
 		if (!this.isServerSocket(socket)) {
 			return;
 		}
+
+		// look for all sockets associated with the user and the channel
 		const { userId, channel } = data;
 		const uidSockets = this.socketsByUidAndChannel[userId];
 		if (uidSockets) {
 			const sockets = uidSockets[channel];
 			if (sockets) {
+				// kick the user out of this channel on these sockets
 				for (const userSocket of sockets) {
 					userSocket.kickOut(channel);
 				}
@@ -220,12 +272,17 @@ class CodeStream {
 		}
 	}
 
+	// handle a request to unsubscribe from a channel, initiated by the client
 	handleUnsubscribe (socket, channel) {
+		// get the user ID associated with this socket
 		const uid = this.uidsBySocketId[socket.id];
+
+		// remove knowledge of this user being subscribed to this channel 
 		if (this.socketsByUidAndChannel[uid]) {
 			delete this.socketsByUidAndChannel[uid][channel];
 		}
 
+		// if the channel is a team channel, remove knowledge of this user being subscribed
 		if (channel.startsWith('team-') && this.uidsByTeamChannel[channel]) {
 			const index = this.uidsByTeamChannel[channel].indexOf(uid);
 			if (index !== -1) {
@@ -234,7 +291,9 @@ class CodeStream {
 		}
 	}
 
+	// handle an incoming message
 	async handleMessage (socket, data) {
+		// messages can only come from the server
 		if (!this.data || !this.isServerSocket(socket)) { return; }
 		try {
 			data = JSON.parse(data);
@@ -246,10 +305,15 @@ class CodeStream {
 		if (!data.channel || !data.data) { 
 			return;
 		}
+
+		// remove any lingering old messages
 		this.sweepOldMessages();
+		
 		if (typeof data.data === 'object') {
 			this.normalizeMessage(data.data);
 		}
+
+		// store the message in our message history for the channel
 		const message = {
 			timestamp: Date.now(),
 			channel: data.channel,
@@ -264,6 +328,8 @@ class CodeStream {
 		}
 	}
 
+	// recursively normalize the message for storage,
+	// since mongo documents can not contain '.' or '$' as keys
 	normalizeMessage (message) {
 		for (const key in message) {
 			let newKey = key;
@@ -289,6 +355,7 @@ class CodeStream {
 		}
 	}
 
+	// remove old messages from the message history
 	sweepOldMessages () {
 		const now = Date.now();
 		const cutoff = now - Config.history.retentionPeriod;
@@ -301,17 +368,20 @@ class CodeStream {
 		this.lastSweep = now;
 	}
 
+	// get the message history for the given set of channels and a given socket
 	async getHistory (socket, request) {
 		if (!this.data) { return; }
 		try {
 			let { channels } = request;
 			const { requestId, since } = request;
 
+			// only serve history in slices of 100 channels
 			if (channels.length > 100) {
 				channels = channels.slice(0, 100);
 			}
 			channels = channels.filter((channel, index, arr) => arr.indexOf(channel) === index);
 
+			// authorize fetching this history
 			const error = await this.authChannelsHistory(socket, channels);
 			if (error) {
 				this.warn(`Auth history request ${requestId} failed: ${error}`);
@@ -330,6 +400,8 @@ class CodeStream {
 					limit: 100
 				}
 			);
+
+			// denormalize the messages and emit them as a history message
 			this.denormalizeMessages(messages);
 			socket.emit('history', {
 				requestId,
@@ -348,9 +420,12 @@ class CodeStream {
 		}
 	}
 
+	// get users subscribed to a team channel
 	async getSubscribedUsers (socket, request) {
 		try {
 			const { channel, requestId } = request;
+
+			// this request is only allowed from the server
 			if (!this.isServerSocket(socket)) {
 				this.warn(`Auth getSubscribedUsers request ${requestId} failed: not a server socket`);
 				return socket.emit('subscribedUsers', { requestId, error: 'access denied' });
@@ -369,6 +444,7 @@ class CodeStream {
 		}
 	}
 
+	// denormalize messages stored on mongo, since mongo does not allow '.' or '$' as keys
 	denormalizeMessages (messages) {
 		for (let message of messages) {
 			delete message._id;
@@ -377,6 +453,8 @@ class CodeStream {
 		}
 	}
 
+	// recursively denormalize a mongo document as a message,
+	// since mongo does not allow '.' or '$' as keys
 	denormalizeMessage (message) {
 		for (const key in message) {
 			let newKey = key;
@@ -399,22 +477,27 @@ class CodeStream {
 		}
 	}
 
+	// authorize a request to fetch message history for a given set of channels
 	async authChannelsHistory (socket, channels) {
+		// get the user associated with the socket
 		const user = await this.getSocketUser(socket);
 		if (!user) { 
 			return 'socket not authenticated, or user not found';
 		}
 
+		// categorize by channel type
 		const channelInfo = this.categorizeChannels(channels);
 		if (!channelInfo) {
 			return 'invalid channels';
 		}
 
+		// only the user can get access to their user channel
 		let unauthedChannel = channelInfo.userChannels.find(channel => channel !== `user-${user._id}`);
 		if (unauthedChannel) {
 			return `unauthorized user channel: ${unauthedChannel}`;
 		}
 
+		// users must be a member of the team channels
 		unauthedChannel = channelInfo.teamChannels.find(channel => {
 			return !user.teamIds.includes(channel.split('team-')[1]);
 		});
@@ -422,9 +505,11 @@ class CodeStream {
 			return `unauthorzed team channel: ${unauthedChannel}`;
 		}
 		
+		// stream channels are authorized separately
 		return await this.authStreamChannelsHistory(channelInfo.streamChannels, user);
 	}
 
+	// authorize a request to fetch message history for a given set of stream channels
 	async authStreamChannelsHistory (channels, user) {
 		if (channels.length === 0) {
 			return;
@@ -432,12 +517,16 @@ class CodeStream {
 		if ((user.teamIds || []).length === 0) {
 			return 'unauthorized stream channel(s)';
 		}
+
+		// fetch the streams
 		const streamIds = channels.map(channel => channel.split('stream-')[1]);
 		const streams = await this.data.streams.getByIds(streamIds);
 		if (streams.length !== streamIds.length) {
 			return 'some streams not found';
 		}
 
+		// stream channels are authorized if they are file or team streams for a team the user is in,
+		// or they are for a stream the user is in
 		if (streams.find(stream => {
 			return (
 				!user.teamIds.includes(stream.teamId) || 
@@ -452,6 +541,7 @@ class CodeStream {
 		}
 	}
 
+	// is this socket a connection to the API server?
 	isServerSocket (socket) {
 		const token = socket.getAuthToken();
 		if (!token) {
@@ -460,6 +550,7 @@ class CodeStream {
 		return token.uid === 'API';
 	}
 
+	// get the user associated with a given socket
 	async getSocketUser (socket) {
 		const token = socket.getAuthToken();
 		if (!token) { 
@@ -468,6 +559,7 @@ class CodeStream {
 		return await this.data.users.getById(token.uid);
 	}
 
+	// categorize channels by type: user, team, and stream
 	categorizeChannels (channels) {
 		const channelInfo = {
 			userChannels: [],
