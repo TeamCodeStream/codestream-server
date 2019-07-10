d7b16347afc6e2ea35e93cd7a6d1fa92ef69ce6a
James Price <jimmyjayp@gmail.com>
2019-07-10 09:46:31 -0400

    added empty structure if microsoft.com integration is not in the config file

--- api_server/config/msteams.js
+++ api_server/config/msteams.js
@@ -4,11 +4,16 @@
 
 let MsTeamsCfg = {};
 if (process.env.CS_API_CFG_FILE) {
-	MsTeamsCfg = require(process.env.CS_API_CFG_FILE).integrations.msteams['microsoft.com'];
+	MsTeamsCfg = require(process.env.CS_API_CFG_FILE).integrations.msteams['microsoft.com'] || {
+		appClientId: null,
+		appClientSecret: null,
+		localProvider: false
+	};
 }
 else {
 	MsTeamsCfg.appClientId = process.env.CS_API_MSTEAMS_CLIENT_ID;
 	MsTeamsCfg.appClientSecret = process.env.CS_API_MSTEAMS_CLIENT_SECRET;
+	MsTeamsCfg.localProvider = false;
 }
 
 module.exports = MsTeamsCfg;
