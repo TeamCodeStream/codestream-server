0e6ea94846c4d36fa4437a5f6ad0255792482603
James Price <jimmyjayp@gmail.com>
2019-07-17 00:17:09 -0400

    Completed initial testing for on-prem template

--- shared/codestream-configs/bin/config-test.js
+++ shared/codestream-configs/bin/config-test.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const structuredCfgFile = require('../lib/load_config');
+const util = require('util')
+const structuredCfgFile = require('../lib/structured_config');
 const testCfg = new structuredCfgFile({
 	schemaFile: '/Users/jj/src/codestream-configs/parameters.json',
 	configFile: 'testcfg.json'
@@ -8,9 +9,10 @@ const testCfg = new structuredCfgFile({
 // testCfg.dump();
 let o;
 
-o = testCfg.getSection('ssl');
+o = testCfg.getSection();
 // o = testCfg.getSection('broadcastEngine.codestreamBroadcaster');
 // o = testCfg.getSection();
 
-console.log(o);
+// console.log(o);
+console.log(util.inspect(o, false, null, true /* enable colors */))
 process.exit();
