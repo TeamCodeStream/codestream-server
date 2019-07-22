7d4155ee45f517c0a1afa18751b99c9b1cf0604c
Colin Stryker <colin@codestream.com>
2019-07-22 15:05:35 -0700

    update copy to already-registered email to indicate where to reset password

--- outbound_email/src/alreadyRegisteredEmailHandler.js
+++ outbound_email/src/alreadyRegisteredEmailHandler.js
@@ -9,7 +9,7 @@ class AlreadyRegisteredEmailHandler extends EmailHandler {
 		this.content = `
 
 <html>
-It looks like you already have an account on CodeStream. Just open CodeStream in your IDE and sign back in. If you don't remember your password, contact us to reset it.<br/><br/>
+It looks like you already have an account on CodeStream. Just open CodeStream in your IDE and sign back in. If you don't remember your password look for the link to reset it on the sign-in form.<br/><br/>
 Team CodeStream<br/>
 </html>
 `;
