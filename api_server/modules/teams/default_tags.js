b1aca3aa832a44b75cc4fe2bade40152fbcdf0dc
Colin Stryker <colin@codestream.com>
2019-08-16 11:04:20 -0700

    update default tags with sort order

--- api_server/modules/teams/default_tags.js
+++ api_server/modules/teams/default_tags.js
@@ -2,27 +2,35 @@
 
 module.exports = {
 	_blue: {
-		color: 'blue'
+		color: 'blue',
+		sortOrder: 1000
 	},
 	_green: {
-		color: 'green'
+		color: 'green',
+		sortOrder: 2000
 	},
 	_yellow: {
-		color: 'yellow'
+		color: 'yellow',
+		sortOrder: 3000
 	},
 	_orange: {
-		color: 'orange'
+		color: 'orange',
+		sortOrder: 4000
 	},
 	_red: {
-		color: 'red'
+		color: 'red',
+		sortOrder: 5000
 	},
 	_purple: {
-		color: 'purple'
+		color: 'purple',
+		sortOrder: 6000
 	},
 	_aqua: {
-		color: 'aqua'
+		color: 'aqua',
+		sortOrder: 7000
 	},
 	_gray: {
-		color: 'gray'
+		color: 'gray',
+		sortOrder: 8000
 	}
 };
