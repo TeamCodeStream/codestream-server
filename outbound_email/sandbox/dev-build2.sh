410b5149e546a950ce7fdee83f3f21ed259a0d60
James Price <jimmyjayp@gmail.com>
2019-09-02 14:09:11 -0400

    build-only environment configuration

--- outbound_email/sandbox/dev-build2.sh
+++ outbound_email/sandbox/dev-build2.sh
@@ -1,19 +1,12 @@
 
+# sandbox configuration for building development asset
+
 export CS_OUTBOUND_EMAIL_ASSET_ENV=dev
 PUBNUB_KEY_FILE="$HOME/.codestream/pubnub/CodeStream-Development-CI_Keyset"
-export CS_OUTBOUND_EMAIL_SQS=${CS_OUTBOUND_EMAIL_ASSET_ENV}_outboundEmail
+export CS_OUTBOUND_EMAIL_SQS=ci_outboundEmail
 
 . $CS_OUTBOUND_EMAIL_TOP/sandbox/defaults.sh
 
-export CS_OUTBOUND_EMAIL_LAMBDA_IAM_ROLE=cs_Lambda
 export CS_OUTBOUND_EMAIL_MONGO_HOST=localhost.codestream.us
 export CS_OUTBOUND_EMAIL_REPLY_TO_DOMAIN=ci.codestream.com
-export CS_OUTBOUND_EMAIL_LAMBDA_TEMPLATE=lambda-func.generic.template.json
-# export CS_OUTBOUND_EMAIL_LAMBDA_SUBNETS='"subnet-c538ff98","subnet-2730ae43"'
-# export CS_OUTBOUND_EMAIL_LAMBDA_SECURITY_GROUPS='"sg-32387241"'
-subnet_id=`dt-aws-tool --map-to-id --subnets csdev_${CS_OUTBOUND_EMAIL_ASSET_ENV}_priv1b --output comma-delim-string-list`
-export CS_OUTBOUND_EMAIL_LAMBDA_SUBNETS="$subnet_id"
-sg_id=`dt-aws-tool --map-to-id --sgs csdev_closed --output comma-delim-string-list`
-export CS_OUTBOUND_EMAIL_LAMBDA_SECURITY_GROUPS=$sg_id
-
 unset CS_OUTBOUND_EMAIL_TO
