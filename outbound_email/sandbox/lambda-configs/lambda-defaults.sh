b4b076a3347f66f3935d29705422091f341a552f
James Price <jimmyjayp@gmail.com>
2019-09-05 18:10:35 -0400

    Adding lambda event source arn to environment

--- outbound_email/sandbox/lambda-configs/lambda-defaults.sh
+++ outbound_email/sandbox/lambda-configs/lambda-defaults.sh
@@ -1,15 +1,13 @@
 
 # lambda function defaults
 
-# this variable's name is generic so applies to all our lambda functions
-[ -z "$CS_FUNCTION_VERSION" ] && export CS_FUNCTION_VERSION="`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/src/package.json -p name`-`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/src/package.json -p version`"
+[ -z "$CS_LAMBDA_VERSION" ] && export CS_LAMBDA_VERSION="`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/src/package.json -p name`-`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/src/package.json -p version`"
 
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_TEMPLATE" ] && export CS_OUTBOUND_EMAIL_LAMBDA_TEMPLATE=lambda-func.generic.template.json
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_RUNTIME" ] && export CS_OUTBOUND_EMAIL_LAMBDA_RUNTIME="nodejs10.x"
 [ -z "$CS_OUTBOUND_EMAIL_AWS_ACCOUNT" ] && export CS_OUTBOUND_EMAIL_AWS_ACCOUNT=564564469595
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_IAM_ROLE" ] && export CS_OUTBOUND_EMAIL_LAMBDA_IAM_ROLE=cs_LambdaDevelopment
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_DESCRIPTION" ] && export CS_OUTBOUND_EMAIL_LAMBDA_DESCRIPTION="outbound email gateway for $CS_OUTBOUND_EMAIL_ENV"
-
 #export CS_OUTBOUND_EMAIL_LAMBDA_SUBNETS=
 #export CS_OUTBOUND_EMAIL_LAMBDA_SECURITY_GROUPS=
 
