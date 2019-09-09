74b434acaf8e7253ee1e5ceb19ba67a4f098d5ec
James Price <jimmyjayp@gmail.com>
2019-09-08 21:41:52 -0400

    now works if sandbox is not loaded

--- api_server/sandbox/docker/api-onprem/settings.sh
+++ api_server/sandbox/docker/api-onprem/settings.sh
@@ -1,8 +1,9 @@
 
 # dockerImageName=""  # default uses directory name as image name
 # dockerBuildDirectory=""
+[ -z "$CS_API_TOP" ] && pkgJsonDir="." || pkgJsonDir=$CS_API_TOP
 dockerHubOrganization=teamcodestream
-defaultImageVersion=`get-json-property -j $CS_API_TOP/package.json -p version`
+defaultImageVersion=`get-json-property -j $pkgJsonDir/package.json -p version`
 buildParameters=""
 runParameters=""
 containerName="csapi"
