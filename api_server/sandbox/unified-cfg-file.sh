2f0a190419488f5283ce71bfad2a93dbf2c9abc0
James Price <jimmyjayp@gmail.com>
2019-09-16 14:34:17 -0400

    added call to shell library function to get codestream cfg file name

--- api_server/sandbox/unified-cfg-file.sh
+++ api_server/sandbox/unified-cfg-file.sh
@@ -10,15 +10,14 @@ export PATH=$CS_API_SANDBOX/node/bin:$CS_API_TOP/node_modules/.bin:$PATH
 
 export PATH=$CS_API_TOP/bin:$PATH
 
-[ -z "$CSSVC_CFG_FILE" -a -z "$CS_API_CFG_FILE" ] && export CS_API_CFG_FILE=$HOME/.codestream/config/codestream-services-config.json
-[ -n "$CSSVC_CFG_FILE" ] && cfgFile=$CSSVC_CFG_FILE || cfgFile=$CS_API_CFG_FILE
-echo "Using config file $cfgFile"
+# this sets CSSVC_CFG_FILE
+sandutil_get_codestream_cfg_file $CS_API_CFG_FILE
 
 # env vars required for aux scripts that don't load the config file
-export CS_API_LOGS=`eval echo $(get-json-property -j $cfgFile -p apiServer.logger.directory)`
-export CS_API_TMP=`eval echo $(get-json-property -j $cfgFile -p apiServer.tmpDirectory)`
-export CS_API_ASSET_ENV=`eval echo $(get-json-property -j $cfgFile -p apiServer.assetEnvironment)`
-export CS_API_ENV=`eval echo $(get-json-property -j $cfgFile -p apiServer.runTimeEnvironment)`
+export CS_API_LOGS=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p apiServer.logger.directory)`
+export CS_API_TMP=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p apiServer.tmpDirectory)`
+export CS_API_ASSET_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p apiServer.assetEnvironment)`
+export CS_API_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p apiServer.runTimeEnvironment)`
 
 # this sets CS_API_CALLBACK_ENV
 set_callback_env
