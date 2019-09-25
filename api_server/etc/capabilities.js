aa9c95fd77836848be0e7d4179a10ab6d09652f7
Colin Stryker <colin@codestream.com>
2019-09-25 16:54:42 -0700

    add multiple markers as a capability

--- api_server/etc/capabilities.js
+++ api_server/etc/capabilities.js
@@ -5,6 +5,10 @@ module.exports = {
 		description: 'SAMPLE',
 		version: '1.0.0'
 	},
+	'multipleMarkers': {
+		description: 'Ability to have multiple code blocks per codemark',
+		version: '1.21.20'
+	},
 	'moveMarkers': {
 		description: 'Ability to change the location of code blocks pointed to by codemarks',
 		version: '1.21.20'
