b668ae55506d901a28c9ceaf92fe51f9cbfe54d9
Colin Stryker <colin@codestream.com>
2019-09-25 14:57:32 -0700

    add moveMarkers as an API capability

--- api_server/etc/capabilities.js
+++ api_server/etc/capabilities.js
@@ -4,5 +4,9 @@ module.exports = {
 	'sample': {
 		description: 'SAMPLE',
 		version: '1.0.0'
+	},
+	'moveMarkers': {
+		description: 'Ability to change the location of code blocks pointed to by codemarks',
+		version: '1.21.20'
 	}
 };
