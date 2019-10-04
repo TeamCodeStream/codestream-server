020f66247bb9de3b11d696073c60f472198ab2e1
James Price <jimmyjayp@gmail.com>
2019-10-04 18:06:53 -0400

    disable lambda support in sandbox if not using the awsSQS queuing engine

--- outbound_email/sandbox/unified-cfg-file.sh
+++ outbound_email/sandbox/unified-cfg-file.sh
@@ -19,10 +19,11 @@ export CS_OUTBOUND_EMAIL_TMP=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -
 export CS_OUTBOUND_EMAIL_ASSET_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.assetEnvironment)`
 export CS_OUTBOUND_EMAIL_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.runTimeEnvironment)`
 export CS_OUTBOUND_EMAIL_SQS=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p queuingEngine.awsSQS.outboundEmailQueueName)`
+[ -z "$CS_OUTBOUND_EMAIL_SQS" ] && echo "AWS SQS is not configured as the queing engine. AWS lambda functions will not be supported."
 
 # when running a VPN, set the mongo connect override to reference mongo via the vpn ip (lambda functions need to connect)
 TUNNEL_IP=$(sandutil_get_tunnel_ip fallbackLocalIp)
 [ -n "$TUNNEL_IP" ] && export CS_OUTBOUND_EMAIL_MONGO_URL=mongodb://$TUNNEL_IP/codestream
 [ -n "$OUTBOUND_EMAIL_MONGO_URL" ] && echo "overriding mongo connection string: $CS_OUTBOUND_EMAIL_MONGO_URL"
 
-. $CS_OUTBOUND_EMAIL_TOP/sandbox/lambda-configs/$CS_OUTBOUND_EMAIL_ENV.sh
+[ -n "$CS_OUTBOUND_EMAIL_SQS" ] && . $CS_OUTBOUND_EMAIL_TOP/sandbox/lambda-configs/$CS_OUTBOUND_EMAIL_ENV.sh
