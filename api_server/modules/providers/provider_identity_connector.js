b76c4636057bdd33834f0699bc74f565c0e832b7
Colin Stryker <colin@codestream.com>
2019-10-07 19:33:12 -0700

    remove deprecation comment

--- api_server/modules/providers/provider_identity_connector.js
+++ api_server/modules/providers/provider_identity_connector.js
@@ -59,8 +59,6 @@ class ProviderIdentityConnector {
 			{ hint: TeamIndexes.byProviderIdentities }
 		);
 
-		// DEPRECATE ME? - pretty sure the notion of being invited to a specific team
-		// that is associated with a provider identity is deprecated now
 		if (this.team) {
 			this.request.log('Matched team ' + this.team.id);
 			if (this.expectedTeamId && this.expectedTeamId !== this.team.id) {
