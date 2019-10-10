da1305ee05595061a3ae155e1cdce5beacbd3f60
James Price <jimmyjayp@gmail.com>
2019-10-10 15:13:33 -0400

    reorganized lambda config

--- outbound_email/sandbox/lambda-configs/lambda-defaults.sh
+++ outbound_email/sandbox/lambda-configs/lambda-defaults.sh
@@ -1,6 +1,14 @@
 
 # lambda function defaults
 
+export CS_OUTBOUND_EMAIL_CFG_FILE=./codestream-services-config.json
+echo -e "Config file reset to $CS_OUTBOUND_EMAIL_CFG_FILE for packaging lambda function\n"
+
+# when running a VPN, set the mongo connect override to reference mongo via the vpn ip (lambda functions need to connect)
+TUNNEL_IP=$(sandutil_get_tunnel_ip fallbackLocalIp)
+[ -n "$TUNNEL_IP" ] && export CS_OUTBOUND_EMAIL_MONGO_URL=mongodb://$TUNNEL_IP/codestream
+[ -n "$OUTBOUND_EMAIL_MONGO_URL" ] && echo "overriding mongo connection string: $CS_OUTBOUND_EMAIL_MONGO_URL"
+
 if [ -z "$CS_LAMBDA_VERSION" ]; then
 	if [ -n "$TCBUILD_ASSET_FULL_NAME" ]; then
 		export CS_LAMBDA_VERSION=$TCBUILD_ASSET_FULL_NAME
@@ -8,34 +16,6 @@ if [ -z "$CS_LAMBDA_VERSION" ]; then
 		export CS_LAMBDA_VERSION="`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/package.json -p name`-`get-json-property -j $CS_OUTBOUND_EMAIL_TOP/package.json -p version`"
 	fi
 fi
-
-if [ -n "$CS_OUTBOUND_EMAIL_CFG_FILE" -o -n "$CSSVC_CFG_FILE" ]; then
-	# For local sandboxes, print a helpful message on how to switch
-	# between running as a lambda function vs. a local node process.
-	if [ "$CS_OUTBOUND_EMAIL_ENV" == local ]; then
-		if [ -z "$CS_OUTBOUND_EMAIL_NO_LAMBDA" ]; then
-			echo "
-Your sandbox is configured for deployment as a lambda function.
-If you want to run it as a node service on this computer run this
-command, kill this shell and re-load your sandbox.
-
-   $ echo CS_OUTBOUND_EMAIL_NO_LAMBDA=1 >> $CS_OUTBOUND_EMAIL_SANDBOX/sb.options
-"
-		else
-			echo "
-Your sandbox is configured to run as a node service on this host. If
-you want to run it as a lambda function, remove the setting line
-from $CS_OUTBOUND_EMAIL_SANDBOX/sb.options, kill the shell and reload
-the sandbox.
-"
-		fi
-	fi
-	# Override the default config file setting for deployment as a lambda
-	# function because the config file needs to be included in the zip
-	# file that gets uploaded to AWS
-	[ -z "$CS_OUTBOUND_EMAIL_NO_LAMBDA" ] && export CS_OUTBOUND_EMAIL_CFG_FILE=./codestream-services-config.json
-fi
-
 
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_TEMPLATE" ] && export CS_OUTBOUND_EMAIL_LAMBDA_TEMPLATE=lambda-func.generic.template.json
 [ -z "$CS_OUTBOUND_EMAIL_LAMBDA_RUNTIME" ] && export CS_OUTBOUND_EMAIL_LAMBDA_RUNTIME="nodejs10.x"
--- outbound_email/sandbox/unified-cfg-file.sh
+++ outbound_email/sandbox/unified-cfg-file.sh
@@ -10,20 +10,44 @@ export PATH=$CS_OUTBOUND_EMAIL_SANDBOX/node/bin:$CS_OUTBOUND_EMAIL_TOP/node_modu
 # sandbox bin directory is at front of search path
 export PATH=$CS_OUTBOUND_EMAIL_TOP/bin:$PATH
 
+# if the run-time env has been set as an option and no config file has been
+# specified, assume this is a spin-up-dev instance and get the latest
+# codestream-cloud development config file
+if [ -n "$CSSVC_ENV" -a -z "$CS_OUTBOUND_EMAIL_CFG_FILE" ]; then
+	export CS_OUTBOUND_EMAIL_CFG_FILE=$(/bin/ls $HOME/.codestream/config/codestream-cloud_dev_*_.json|tail -1)
+	export CS_OUTBOUND_EMAIL_ENV=$CSSVC_ENV
+fi
+
 # find the config file
 sandutil_get_codestream_cfg_file "$CS_OUTBOUND_EMAIL_SANDBOX" "$CS_OUTBOUND_EMAIL_CFG_FILE"
 
 # env vars required for scripts that don't load the config file
+[ -z "$CS_OUTBOUND_EMAIL_ENV" ] && export CS_OUTBOUND_EMAIL_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.runTimeEnvironment)`
 export CS_OUTBOUND_EMAIL_LOGS=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.logger.directory)`
 export CS_OUTBOUND_EMAIL_TMP=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.tmpDirectory)`
 export CS_OUTBOUND_EMAIL_ASSET_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.assetEnvironment)`
-export CS_OUTBOUND_EMAIL_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p outboundEmailServer.runTimeEnvironment)`
 export CS_OUTBOUND_EMAIL_SQS=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p queuingEngine.awsSQS.outboundEmailQueueName)`
-[ -z "$CS_OUTBOUND_EMAIL_SQS" ] && echo "AWS SQS is not configured as the queing engine. AWS lambda functions will not be supported."
+[ -z "$CS_OUTBOUND_EMAIL_SQS" ] && echo "AWS SQS is not configured as the queing engine. AWS lambda functions will not be supported." && export CS_OUTBOUND_EMAIL_NO_LAMBDA=1
 
-# when running a VPN, set the mongo connect override to reference mongo via the vpn ip (lambda functions need to connect)
-TUNNEL_IP=$(sandutil_get_tunnel_ip fallbackLocalIp)
-[ -n "$TUNNEL_IP" ] && export CS_OUTBOUND_EMAIL_MONGO_URL=mongodb://$TUNNEL_IP/codestream
-[ -n "$OUTBOUND_EMAIL_MONGO_URL" ] && echo "overriding mongo connection string: $CS_OUTBOUND_EMAIL_MONGO_URL"
+# For local sandboxes, print a helpful message on how to switch
+# between running as a lambda function vs. a local node process.
+if [ "$CS_OUTBOUND_EMAIL_ENV" == local ]; then
+	if [ -z "$CS_OUTBOUND_EMAIL_NO_LAMBDA" ]; then
+		echo "
+Your sandbox is configured for deployment as a lambda function.
+If you want to run it as a node service on this computer run this
+command, kill this shell and re-load your sandbox.
 
-[ -n "$CS_OUTBOUND_EMAIL_SQS" ] && . $CS_OUTBOUND_EMAIL_TOP/sandbox/lambda-configs/$CS_OUTBOUND_EMAIL_ENV.sh
+$ echo CS_OUTBOUND_EMAIL_NO_LAMBDA=1 >> $CS_OUTBOUND_EMAIL_SANDBOX/sb.options
+"
+	else
+		echo "
+Your sandbox is configured to run as a node service on this host. If
+you want to run it as a lambda function, remove the setting line
+from $CS_OUTBOUND_EMAIL_SANDBOX/sb.options, kill the shell and reload
+the sandbox.
+"
+	fi
+fi
+
+[ -n "$CS_OUTBOUND_EMAIL_SQS" -a -z "$CS_OUTBOUND_EMAIL_NO_LAMBDA" ] && . $CS_OUTBOUND_EMAIL_TOP/sandbox/lambda-configs/$CS_OUTBOUND_EMAIL_ENV.sh
