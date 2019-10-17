cabde4841e08a4c4da9731cf589c864fd0651e2c
James Price <jimmyjayp@gmail.com>
2019-10-17 14:19:58 -0400

    Docker build support for relocation of package.json

--- outbound_email/sandbox/docker/mailout-onprem/settings.sh
+++ outbound_email/sandbox/docker/mailout-onprem/settings.sh
@@ -3,7 +3,7 @@
 # dockerBuildDirectory=""
 [ -z "$CS_OUTBOUND_EMAIL_TOP" ] && pkgJsonDir="." || pkgJsonDir=$CS_OUTBOUND_EMAIL_TOP
 dockerHubOrganization=teamcodestream
-defaultImageVersion=`get-json-property -j $pkgJsonDir/src/package.json -p version`
+defaultImageVersion=`get-json-property -j $pkgJsonDir/package.json -p version`
 buildParameters=""
 runParameters=""
 containerName="csmailout"
