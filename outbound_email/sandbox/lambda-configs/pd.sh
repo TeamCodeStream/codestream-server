cff846fd836f68ca4cef83b706fe02cee70adbf5
James Price <jimmyjayp@gmail.com>
2019-10-17 12:00:36 -0400

    PD needs to override mongo port

--- outbound_email/sandbox/lambda-configs/pd.sh
+++ outbound_email/sandbox/lambda-configs/pd.sh
@@ -2,3 +2,5 @@
 export CS_OUTBOUND_EMAIL_LAMBDA_IAM_ROLE=cs_Lambda
 export CS_OUTBOUND_EMAIL_LAMBDA_SUBNETS='"subnet-c538ff98","subnet-2730ae43"'
 export CS_OUTBOUND_EMAIL_LAMBDA_SECURITY_GROUPS='"sg-32387241"'
+. /home/web/.codestream/mongo/pd-codestream-api
+export CSSVC_MONGO_URL=mongodb://$MONGO_HOST:$MONGO_PORT/$MONGO_DB
