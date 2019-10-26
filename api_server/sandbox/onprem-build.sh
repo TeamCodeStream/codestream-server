2fdb76df6231f8167b6f1decbe8e70222c86e8b4
James Price <jimmyjayp@gmail.com>
2019-10-26 18:14:30 -0400

    A little housekeeping and added temporary unified-cfg sandbox config files for qa & prod

--- api_server/sandbox/onprem-build.sh
+++
@@ -1,4 +0,0 @@
-
-. $CS_API_TOP/sandbox/defaults.sh
-
-export CS_API_DONT_WANT_AWS=1
---
+++ api_server/sandbox/qa2.sh
@@ -0,0 +1,5 @@
+
+# qa
+export CSSVC_ENV=qa
+export CSSVC_CONFIGURATION=codestream-cloud
+. $CS_API_TOP/sandbox/unified-cfg-file.sh
