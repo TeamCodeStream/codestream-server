40e960ffe7f67fc8830eb8d3e982375c7a7cf213
James Price <jimmyjayp@gmail.com>
2019-11-13 01:24:13 -0500

    PD now functions like any spin-up dev environment

--- outbound_email/sandbox/lambda-configs/pd.sh
+++
@@ -1,6 +0,0 @@
-
-export CS_OUTBOUND_EMAIL_LAMBDA_IAM_ROLE=cs_Lambda
-export CS_OUTBOUND_EMAIL_LAMBDA_SUBNETS='"subnet-c538ff98","subnet-2730ae43"'
-export CS_OUTBOUND_EMAIL_LAMBDA_SECURITY_GROUPS='"sg-32387241"'
-. /home/web/.codestream/mongo/pd-codestream-api
-export CS_OUTBOUND_EMAIL_MONGO_URL=mongodb://$MONGO_HOST:$MONGO_PORT/$MONGO_DB
