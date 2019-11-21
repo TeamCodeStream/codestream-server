1650019d78306615b5b0dc02ea1ba158731bf7f6
Colin Stryker <colin@codestream.com>
2019-11-21 14:49:28 -0800

    disable provider-action tests having to do with with slack callback verification, until we re-enable that

--- api_server/modules/providers/test/provider_action/test.js
+++ api_server/modules/providers/test/provider_action/test.js
@@ -5,7 +5,7 @@
 
 const ProviderActionTest = require('./provider_action_test');
 const TrackingTest = require('./tracking_test');
-const PayloadRequiredTest = require('./payload_required_test');
+//const PayloadRequiredTest = require('./payload_required_test');
 const NoActionIdTest = require('./no_action_id_test');
 const InvalidActionIdTest = require('./invalid_action_id_test');
 const NoUserIdTest = require('./no_user_id_test');
@@ -14,8 +14,8 @@ const NoTeamIdTest = require('./no_team_id_test');
 const UserNotOnTeamTest = require('./user_not_on_team_test');
 const TeamNotFoundTest = require('./team_not_found_test');
 const UnknownLinkTypeTest = require('./unknown_link_type_test');
-const InvalidSignatureTest = require('./invalid_signature_test');
-const ImproperSignatureTest = require('./improper_signature_test');
+//const InvalidSignatureTest = require('./invalid_signature_test');
+//const ImproperSignatureTest = require('./improper_signature_test');
 
 const PROVIDERS = [
 	'slack'
@@ -33,7 +33,7 @@ class ProviderActionRequestTester {
 			new TrackingTest({ provider, linkType: 'ide' }).test();
 			new TrackingTest({ provider, linkType: 'external', externalType: 'code' }).test();
 			new TrackingTest({ provider, linkType: 'external', externalType: 'issue' }).test();
-			new PayloadRequiredTest({ provider }).test();
+			//new PayloadRequiredTest({ provider }).test();
 			new NoActionIdTest({ provider }).test();
 			new InvalidActionIdTest({ provider }).test();
 			new NoUserIdTest({ provider }).test();
@@ -42,8 +42,8 @@ class ProviderActionRequestTester {
 			new UserNotOnTeamTest({ provider }).test();
 			new TeamNotFoundTest({ provider }).test();
 			new UnknownLinkTypeTest({ provider }).test();
-			new InvalidSignatureTest({ provider }).test();
-			new ImproperSignatureTest({ provider }).test();
+			//new InvalidSignatureTest({ provider }).test();
+			//new ImproperSignatureTest({ provider }).test();
 		});
 	}
 }
