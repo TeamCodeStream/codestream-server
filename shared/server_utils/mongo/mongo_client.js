37dcb1d6a5ffee318d3263c1f208bc5bf8781345
Colin Stryker <colin@codestream.com>
2020-02-07 13:50:34 -0800

    add useUnifiedTopology to mongo init

--- shared/server_utils/mongo/mongo_client.js
+++ shared/server_utils/mongo/mongo_client.js
@@ -80,7 +80,10 @@ class MongoClient {
 			if (this.config.logger) {
 				this.config.logger.log(`Connecting to mongo: ${this.config.url}`);
 			}
-			const settings = Object.assign({}, this.config.settings, { useNewUrlParser: true });
+			const settings = Object.assign({}, this.config.settings, {
+				useNewUrlParser: true,
+				useUnifiedTopology: true
+			});
 			if (this.options.tryIndefinitely) {
 				await TryIndefinitely(async () => {
 					this.mongoClient = await MongoDbClient.connect(
