86b48fd2645219cd720d6f8695dd3e0c47e9f377
Colin Stryker <colin@codestream.com>
2020-02-27 17:23:52 -0800

    fix typo

--- api_server/modules/web/web_unfollow_error_request.js
+++ api_server/modules/web/web_unfollow_error_request.js
@@ -12,7 +12,7 @@ class WebUnfollowErrorRequest extends WebRequestBase {
 		const errorCode = decodeURIComponent(this.request.query.error || '');
 		return super.render('error', {
 			title: 'Request failed',
-			body: `You request to unfollow this codemark failed (error code <b>${errorCode}</b>). <a href="mailto:support@codestream.com">Contact support</a> if you need assistance.`
+			body: `Your request to unfollow this codemark failed (error code <b>${errorCode}</b>). <a href="mailto:support@codestream.com">Contact support</a> if you need assistance.`
 		});
 	}
 }
