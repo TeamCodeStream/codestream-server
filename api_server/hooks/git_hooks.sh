e71002038d7da45c7f950aed0b586a1fa5a45de0
James Price <jimmyjayp@gmail.com>
2020-03-21 15:02:32 -0400

    git hook will call sandbox/config-sandbox hook if git submodules were updated (report status as modified)

--- api_server/hooks/git_hooks.sh
+++ api_server/hooks/git_hooks.sh
@@ -1,31 +1,41 @@
 
 # Common shell functions for use in git hooks
 
-# return 1 if file has changed, 0 if not
+# return 0 if file has changed, 1 if not
 githook_file_has_changed() {
     local file=$1
     echo -n "Checking $file..."
     if [[ $(git diff HEAD@{1}..HEAD@{0} -- "${file}" | wc -l) -gt 0 ]]; then
         echo "modified"
-        return 1
+        return 0
     fi
     echo "not modified"
-    return 0
+    return 1
 }
 
+# return 0 if any modues have been modified
+githook_module_is_modified() {
+    local submod
+    local tmpFile=/tmp/modupdated$$
+    local rc=1
+    for submod in `cat .gitmodules|grep '^\[submodule'|cut -f2 -d\"`; do
+        [ ! -f /tmp/updated-mods ] && git status -s | grep "^ M" >$tmpFile
+        grep -q "$submod$" $tmpFile && rc=0 && echo "git submodule $submod updated" && break
+    done
+    /bin/rm $tmpFile
+    return $rc
+}
+
+# call the config-sandbox script if package.json, package-lock.json or yarn.lock changed or
+# if any git submodules' status is modified
 githook_reconfigure_sandbox() {
-    local update_modules=0
     [ -z "$CS_API_TOP" ] && echo "CS_API_TOP not set" && return
-    githook_file_has_changed package.json; [ $? -eq 1 ] && update_modules=1
-    if [[ "$CS_API_YARN" == true ]]; then
-        githook_file_has_changed yarn.lock; [ $? -eq 1 ] && update_modules=1
-	elif [ -f package-lock.json ]; then
-		githook_file_has_changed package-lock.json; [ $? -eq 1 ] && update_modules=1
-    fi
-    if [[ $update_modules -eq 1 ]]; then
-        echo "Rebuilding node_modules"
-        $CS_API_TOP/sandbox/configure-sandbox git-hook
-    fi
+    local call_sandbox_config=0
+    githook_module_is_modified && call_sandbox_config=1
+    [ $call_sandbox_config -eq 0  -a  -f package.json ] && githook_file_has_changed package.json && call_sandbox_config=1
+	[ $call_sandbox_config -eq 0  -a  -f package-lock.json && githook_file_has_changed package-lock.json && call_sandbox_config=1
+    [ $call_sandbox_config -eq 0  -a  "$CS_API_YARN" == "true" ] && githook_file_has_changed yarn.lock && call_sandbox_config=1
+    [ $call_sandbox_config -eq 1 ] && echo "executing sandbox/configure-sandbox git-hook" && sandbox/configure-sandbox git-hook
 }
 
 [ "$CS_API_DISABLE_GIT_HOOKS" == 1 ] && echo "hooks disabled (CS_API_DISABLE_GIT_HOOKS=1)" && exit 0
