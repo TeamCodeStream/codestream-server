d680249cca5ec3bcea1ddfd514bfdd8666c7584d
James Price <jimmyjayp@gmail.com>
2020-03-28 11:10:26 -0400

    Cleanup config code which now requires structured cfg file module

--- broadcaster/config.js
+++ broadcaster/config.js
@@ -4,29 +4,34 @@
 
 /* eslint no-console: 0 */
 
-const structuredCfgFile = require('./codestream-configs/lib/structured_config');
+const StructuredCfgFile = require('./codestream-configs/lib/structured_config');
 
-let MongoCfg = {};
-let LoggerCfg = {
-	basename: 'broadcaster',								// use this for the basename of the log file
-	retentionPeriod: 30 * 24 * 60 * 60 * 1000			// retain log files for this many milliseconds
-};
-let Secrets = {};
-let HttpsCfg = {};
-let CfgFileName = process.env.CS_BROADCASTER_CFG_FILE || process.env.CSSVC_CFG_FILE;
-const CfgData = new structuredCfgFile({
-	schemaFile: './codestream-configs/parameters.json',
-	configFile: CfgFileName
-});
-MongoCfg = CfgData.getSection('storage.mongo');
+let CfgOpts;
+if (process.env.CS_BROADCASTER_CFG_FILE || process.env.CSSVC_CFG_FILE) {
+	CfgOpts = { configFile: process.env.CS_BROADCASTER_CFG_FILE || process.env.CSSVC_CFG_FILE };
+}
+else if (process.env.CSSVC_CFG_URL) {
+	CfgOpts = { mongoUrl: process.env.CSSVC_CFG_URL };
+}
+else {
+	console.log('no configuration provided. Set CSSVC_CFG_FILE or CSSVC_CFG_URL.');
+	process.exit(1);
+}
+const CfgData = new StructuredCfgFile(CfgOpts);
+
+let MongoCfg = CfgData.getSection('storage.mongo');
 MongoCfg.database = CfgData._mongoUrlParse(MongoCfg.url).database;
 
-LoggerCfg = { ...LoggerCfg, ...CfgData.getSection('broadcastEngine.codestreamBroadcaster.logger') };
+let LoggerCfg = { 
+	basename: 'broadcaster',								// use this for the basename of the log file
+	retentionPeriod: 30 * 24 * 60 * 60 * 1000,				// retain log files for this many milliseconds
+	...CfgData.getSection('broadcastEngine.codestreamBroadcaster.logger')
+};
 
-Secrets = CfgData.getSection('broadcastEngine.codestreamBroadcaster.secrets');
+let Secrets = CfgData.getSection('broadcastEngine.codestreamBroadcaster.secrets');
 Secrets.subscriptionCheat = CfgData.getProperty('sharedSecrets.subscriptionCheat');
 
-HttpsCfg = CfgData.getSection('ssl');
+let HttpsCfg = CfgData.getSection('ssl');
 HttpsCfg.port = CfgData.getProperty('broadcastEngine.codestreamBroadcaster.port').toString();
 HttpsCfg.ignoreHttps = CfgData.getProperty('broadcastEngine.codestreamBroadcaster.ignoreHttps');
 
@@ -41,5 +46,8 @@ const Cfg = {
 	}
 };
 
-if (process.env.CS_BROADCASTER_SHOW_CFG) console.log('Config:', Cfg);
+if (CfgData.getSection('broadcastEngine.codestreamBroadcaster.showConfig')) {
+	console.log('Config[broadcaster]:', JSON.stringify(Cfg, undefined, 10));
+}
+
 module.exports = Cfg;
