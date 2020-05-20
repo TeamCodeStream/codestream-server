fb5251b54c1ede3ebc5c6a249fe0fdb017a3c873
James Price <jimmyjayp@gmail.com>
2020-05-19 20:33:15 -0400

    attempt at updating mocha tests with structured config

--- shared/server_utils/pubnub/test/test.js
+++ shared/server_utils/pubnub/test/test.js
@@ -12,24 +12,34 @@ const PresenceTest = require('./presence_test');
 const LeaveTest = require('./leave_test');
 const ApiConfig = require(process.env.CS_API_TOP + '/config/config');
 
+var Config;
+
 // make eslint happy
-/* globals describe */
+/* globals describe, before */
 
 describe('pubnub', function() {
 
-	if (ApiConfig.getPreferredConfig().socketCluster.port) {
-		console.log('NOTE - Pubnub tests cannot pass if SocketCluster is enabled, ignoring');
+	before(async () => {
+		Config = await ApiConfig.loadPreferredConfig();
+		console.log('WE ARE HERE');
+		await new Promise(resolve => {
+			setTimeout(resolve, 5000);
+		});
+	});
+
+	if (Config.whichBroadcastEngine !== 'pubnub') {
+		console.log('NOTE - Pubnub tests cannot pass if pubnub is not enabled, ignoring');
 		return;
 	}
 
 	this.timeout(30000);
 
 	new PubNubTest().test();
-	new NoAccessTest().test();
-	// new RevokeAccessTest().test();	// Disabled pending resolution of COD-64
-	new UnsubscribeTest().test();
-	// new HistoryTest().test();
-	// new MultipleHistoryTest().test();	// Disabled pending resolution of COD-135
-	new PresenceTest().test();
-	new LeaveTest().test();
+	// new NoAccessTest().test();
+	// // new RevokeAccessTest().test();	// Disabled pending resolution of COD-64
+	// new UnsubscribeTest().test();
+	// // new HistoryTest().test();
+	// // new MultipleHistoryTest().test();	// Disabled pending resolution of COD-135
+	// new PresenceTest().test();
+	// new LeaveTest().test();
 });
