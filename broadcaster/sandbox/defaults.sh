4cbb538ae68ad54c7d23200b82cee2442cf38d8b
James Price <jimmyjayp@gmail.com>
2020-05-28 09:17:03 -0400

    Rebased codestream-configs and made sandbox DOA based on asset env value

--- broadcaster/sandbox/defaults.sh
+++ broadcaster/sandbox/defaults.sh
@@ -17,4 +17,4 @@ sandutil_get_codestream_cfg_file "$CS_BROADCASTER_SANDBOX" "$configParm" "$CSSVC
 export CS_BROADCASTER_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p sharedGeneral.runTimeEnvironment 2>/dev/null)`
 export CS_BROADCASTER_LOGS=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p broadcastEngine.codestreamBroadcaster.logger.directory 2>/dev/null)`
 export CS_BROADCASTER_ASSET_ENV=`eval echo $(get-json-property -j $CSSVC_CFG_FILE -p broadcastEngine.codestreamBroadcaster.assetEnvironment 2>/dev/null)`
-[ -z "$CS_BROADCASTER_ENV" ] && echo "The config file does not support the codestream broadcaster as a broadcastEngine. This sandbox is DOA."
+[ -z "$CS_BROADCASTER_ASSET_ENV" ] && echo "The config file does not support the codestream broadcaster as a broadcastEngine. This sandbox is DOA."
