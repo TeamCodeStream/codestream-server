e878183786ab7926542595b1e8416ef4ac76401c
Colin Stryker <colin@codestream.com>
2020-06-04 18:55:42 -0700

    debugging

--- api_server/modules/teams/test/get_team/get_team_request_tester.js
+++ api_server/modules/teams/test/get_team/get_team_request_tester.js
@@ -2,23 +2,19 @@
 
 'use strict';
 
-/*
 const GetTeamTest = require('./get_team_test');
 const GetOtherTeamTest = require('./get_other_team_test');
 const NotFoundTest = require('./not_found_test');
 const ACLTest = require('./acl_test');
-*/
 const StandardProviderHostsTest = require('./standard_provider_hosts_test');
 
 class GetTeamRequestTester {
 
 	getTeamTest () {
-		/*
 		new GetTeamTest().test();
 		new GetOtherTeamTest().test();
 		new NotFoundTest().test();
 		new ACLTest().test();
-		*/
 		new StandardProviderHostsTest().test();
 	}
 }
