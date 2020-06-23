fdcc30e3a3c52729e277223dfe350aa2b73f07f6
Colin Stryker <colin@codestream.com>
2020-06-23 08:40:51 -0700

    open up kickstart to all ides again

--- api_server/etc/capabilities.js
+++ api_server/etc/capabilities.js
@@ -58,8 +58,7 @@ module.exports = {
 	'kickstart': {
 		description: 'Kickstart features',
 		version: '7.4.2',
-		restricted: true,
-		supportedIdes: ['VS Code', 'VS']
+		restricted: true
 	},
 	'cr2pr': {
 		description: 'Features supporting opening pull requests after code reviews',
