945791a000176ffb3c33298b55198ee14356ddcb
Colin Stryker <colin@codestream.com>
2020-07-07 20:40:16 -0700

    add bitbucket server files

---
+++ api_server/modules/bitbucket_server_auth/bitbucket_server_auth.js
@@ -0,0 +1,27 @@
+// provide service to handle bitbucket server credential authorization
+
+'use strict';
+
+const OAuthModule = require(process.env.CSSVC_BACKEND_ROOT + '/api_server/lib/oauth/oauth_module.js');
+
+const OAUTH_CONFIG = {
+	provider: 'bitbucket_server',
+	host: 'bitbucket/server',
+	apiHost: 'api.bitbucket.org/2.0',
+	authPath: 'site/oauth2/authorize',
+	tokenPath: 'site/oauth2/access_token',
+	exchangeFormat: 'form',
+	scopes: 'projects:read repositories:write',
+	appIdInAuthorizationHeader: true,
+	forEnterprise: true
+};
+
+class BitbucketServerAuth extends OAuthModule {
+
+	constructor (config) {
+		super(config);
+		this.oauthConfig = OAUTH_CONFIG;
+	}
+}
+
+module.exports = BitbucketServerAuth;
---
+++ api_server/modules/bitbucket_server_auth/module.js
@@ -0,0 +1,3 @@
+'use strict';
+
+module.exports = require('./bitbucket_server_auth.js');
