555841d59d58d86fd541bd412c916f2d68d78410
Colin Stryker <colin@codestream.com>
2020-08-24 11:45:13 -0700

    temporarily remove notifications scope for testing

--- api_server/modules/github_auth/github_auth.js
+++ api_server/modules/github_auth/github_auth.js
@@ -12,7 +12,7 @@ const OAUTH_CONFIG = {
 	authPath: 'login/oauth/authorize',
 	tokenPath: 'login/oauth/access_token',
 	exchangeFormat: 'query',
-	scopes: 'repo,read:user,user:email,notifications',
+	scopes: 'repo,read:user,user:email',
 	noGrantType: true,
 	hasIssues: true,
 	supportsSignup: true
