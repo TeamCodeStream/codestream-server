30b942ef68fa33a18e80b2a61d41e82aa3500e85
James Price <jimmyjayp@gmail.com>
2020-09-07 12:14:31 -0400

    structured config file reference takes precedence over config stored in mongo

--- shared/codestream_configs/lib/structured_config.js
+++ shared/codestream_configs/lib/structured_config.js
@@ -6,11 +6,14 @@ const StructuredConfigMongo = require('./structured_config_mongo');
 
 class StructuredConfigFactory {
 	create(options) {
-		if (options.mongoUrl) {
+		if (options.configFile) {
 			return new StructuredConfigMongo(options);
 		}
-		else {
+		else if (options.mongoUrl) {
 			return new StructuredConfigFile(options);
+		}
+		else {
+			console.error("StructuredConfigFactory(): missing config reference option. 'configFile' or 'mongoUrl' option required");
 		}
 	}
 }
