b093b586e1166b89a1fbdbe82e562956c60d2a4b
James Price <jimmyjayp@gmail.com>
2020-09-10 14:50:22 -0400

    config file takes precendence over config stored in mongo

--- shared/codestream_configs/lib/structured_config.js
+++ shared/codestream_configs/lib/structured_config.js
@@ -5,12 +5,15 @@ const StructuredConfigFile = require('./structured_config_file');
 const StructuredConfigMongo = require('./structured_config_mongo');
 
 class StructuredConfigFactory {
-	create(options) {
-		if (options.mongoUrl) {
+	create(options={}) {
+		if (options.configFile) {
+			return new StructuredConfigFile(options);
+		}
+		else if(options.mongoUrl) {
 			return new StructuredConfigMongo(options);
 		}
 		else {
-			return new StructuredConfigFile(options);
+			console.error('you need a config reference (configFile or mongoUrl) to get a config from the factory')
 		}
 	}
 }
