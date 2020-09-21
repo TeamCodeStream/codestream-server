66a762f900c77c0d6309ca75a97ed4c2872a2d80
Colin Stryker <colin@codestream.com>
2020-09-21 14:51:31 -0700

    debugging inbound email tests

--- inbound_email/test/email_tests.js
+++ inbound_email/test/email_tests.js
@@ -7,7 +7,6 @@ module.exports = [
 		emailFile: 'simple',
 		expectedText: 'test'
 	},
-	/*
 	{
 		description: 'an email with no from address',
 		emailFile: 'no_from',
@@ -103,5 +102,4 @@ module.exports = [
 		emailFile: 'no_text',
 		shouldFail: true
 	}
-	*/
 ];
