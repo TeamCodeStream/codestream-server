6c0f26114adf8c03ca5f870fd150712b19c64863
James Price <jimmyjayp@gmail.com>
2021-02-24 13:04:10 -0500

    Fix phone home checkbox (again)

--- onprem_admin/api/index.js
+++ onprem_admin/api/index.js
@@ -5,6 +5,7 @@ import Passport from 'passport';
 import { Logger, MongoStructuredConfig, AdminConfig, SystemStatusMonitor, MongoClient } from '../config/globalData';
 import AdminAccess from '../lib/adminAccess';
 import GetAssetData from '../../shared/server_utils/get_asset_data';
+import GetLicenseData from '../lib/getLicenseData';
 
 const ApiRouter = Express.Router();
 
@@ -91,24 +92,7 @@ ApiRouter.post('/no-auth/register', async (req, res) => {
 // a list of unique license plans found across the entire companies collection
 ApiRouter.get('/licenses', async (req, res) => {
 	Logger.log(`api(get):/licenses`);
-	const licenseData = await MongoClient.db()
-		.collection('companies')
-		.find({ plan: { $exists: true } })
-		.sort({ plan: -1 })
-		.toArray();
-	const licenses = {};
-	let isTrial = false;
-	// let isExpired = false;
-	licenseData.forEach(company => {
-		const thisLicense = company.plan === '14DAYTRIAL' ? 'TRIAL' : company.plan;
-		licenses[thisLicense] = 1;
-		if (thisLicense.match(/trial/i)) isTrial = true;
-	});
-	// the license doesn't get added to the database until after the first codemark
-	// is created so we assume a 14DAYTRIAL license for a pre-used database.
-	// if (!Object.keys(licenses).length) licenses.push('14DAYTRIAL');
-	if (!Object.keys(licenses).length) licenses.push('TRIAL');
-	res.send({ licenses: Object.keys(licenses), isTrial });
+	res.send(await GetLicenseData(MongoClient));
 });
 
 
