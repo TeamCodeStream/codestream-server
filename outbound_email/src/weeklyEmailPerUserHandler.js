3adebe7f0212acd8e9025ab81123d357639660f3
Colin Stryker <colin@codestream.com>
2021-03-08 12:42:38 -0800

    set weekly email run to happen at 5 PM ET, and only actually send emails to codestream.com, for testing in prod

--- outbound_email/src/weeklyEmailPerUserHandler.js
+++ outbound_email/src/weeklyEmailPerUserHandler.js
@@ -457,7 +457,7 @@ class WeeklyEmailPerUserHandler {
 			requestId: this.requestId
 		};
 		try {
-			if (true/*this.user.email.match(/.*@codestream\.com$/)*/) {
+			if (this.user.email.match(/.*@codestream\.com$/)) {
 				this.logger.log(`Sending weekly email to ${this.user.email}...`);
 				await this.sender.sendEmail(options);
 			} else {
