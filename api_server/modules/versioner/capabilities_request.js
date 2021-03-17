fb64b198c180d006d267563c4e089cdf2f65878c
Colin Stryker <colin@codestream.com>
2021-03-17 13:06:14 -0700

    return runTimeEnvironment and isOnPrem with capabilities response

--- api_server/modules/versioner/capabilities_request.js
+++ api_server/modules/versioner/capabilities_request.js
@@ -15,7 +15,9 @@ class CapabilitiesRequest extends RestfulRequest {
 	async process () {
 		// return the capabilities
 		this.responseData = {
-			capabilities: { ...APICapabilities }
+			capabilities: { ...APICapabilities },
+			runTimeEnvironment: this.api.config.sharedGeneral.runTimeEnvironment,
+			isOnPrem: this.api.config.sharedGeneral.isOnPrem
 		};
 	}
 
