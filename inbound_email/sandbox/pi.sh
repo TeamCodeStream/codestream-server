ceb9fb9a3dce148a7b8eb6a4aaf3d73d6aedd492
James Price <jimmyjayp@gmail.com>
2021-05-13 00:34:43 -0400

    fix asset env and add pd secrets to config

--- inbound_email/sandbox/pi.sh
+++ inbound_email/sandbox/pi.sh
@@ -1,6 +1,6 @@
 
 # use for production integration builds
-[ -z "$CS_MAILIN_ASSET_ENV" ] && export CS_MAILIN_ASSET_ENV=dev
+[ -z "$CS_MAILIN_ASSET_ENV" ] && export CS_MAILIN_ASSET_ENV=prod
 export CSSVC_ENV=pi
 export CSSVC_CONFIGURATION=codestream-cloud
 . $CS_MAILIN_TOP/sandbox/defaults.sh
