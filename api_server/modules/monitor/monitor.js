bbda46b051faae7bb57c54364eb7522bfa5d0daf
Colin Stryker <cstryker@newrelic.com>
2021-07-07 16:25:17 -0700

    restructure newrelic inclusion

--- api_server/modules/monitor/monitor.js
+++ api_server/modules/monitor/monitor.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const APIServerModule = require(process.env.CSSVC_BACKEND_ROOT + '/api_server/lib/api_server/api_server_module');
+const NewRelic = require('newrelic');
 
 class NewRelicModule extends APIServerModule {
 
 	services () {
 		return async () => {
-			this.newrelic = this.newrelic || require('newrelic');
+			this.newrelic = NewRelic;
 			return { newrelic: this.newrelic };
 		};
 	}
