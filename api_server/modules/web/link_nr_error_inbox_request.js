d4ebc6e4906a34d6fa1ee70a83c639c4053e1596
Brian <brian@codestream.com>
2021-07-28 20:07:28 +0000

    Web page to redirect into IDE

--- api_server/modules/web/link_nr_error_inbox_request.js
+++ api_server/modules/web/link_nr_error_inbox_request.js
@@ -2,7 +2,7 @@
 
 'use strict';
 const WebRequestBase = require('./web_request_base');
-const { ides} = require('./config');
+const { defaultCookieName, ides} = require('./config');
 
 class LinkNrErrorInboxRequest extends WebRequestBase {
 	async authorize () {
@@ -19,17 +19,18 @@ class LinkNrErrorInboxRequest extends WebRequestBase {
 	}
  
 	async render () {
-		const templateProps = {			 
+ 		const templateProps = {			 
 			launchIde: this.request.query.ide === ''
 					? 'default'
-					: this.request.query.ide,	
+					: this.request.query.ide,
 			queryStringFull: JSON.stringify(this.request.query),	 
 			queryString: {			 		 
 				ide: this.request.query.ide === ''
 						? 'default'
 						: this.request.query.ide, 
 			},			 
-			icons: {},			 
+			icons: {},	
+			// if we ever get a repoId pass it here		 
 			partial_launcher_model: this.createLauncherModel(""),
 			partial_title_model: { },
 			segmentKey: this.api.config.telemetry.segment.webToken
@@ -39,12 +40,37 @@ class LinkNrErrorInboxRequest extends WebRequestBase {
 
 	createLauncherModel (repoId) {
 		// overwriting the base since the base deals with mongo data
-		let result = {		 
+
+		const cookieNames = [];
+		if (repoId) {
+			cookieNames.push(`${defaultCookieName}--${repoId}`);
+		}
+		cookieNames.push(defaultCookieName);
+		const queryStringIDE = this.request.query && this.request.query.ide;
+		let autoOpen = !!(!queryStringIDE || queryStringIDE === 'default');
+		const lastOrigin = ((function() {
+			for (const cookieName of cookieNames) {
+				const cookie = this.request.cookies && this.request.cookies[cookieName];
+				if (cookie) {
+					const mappedIde = ides.find(_ => _.moniker === cookie);
+					if (mappedIde) {
+						return { lastOrigin: mappedIde, autoOpen: autoOpen, isMru: true };
+					}
+				}
+			}
+			return {
+				lastOrigin: ides.find(_ => _.moniker === "vsc"),
+				autoOpen: false,
+				isMru: false
+			}
+		}).bind(this))();
+		const result = {		 
 			ides: ides,
+			csrf: this.request.csrfToken(),
 			src: decodeURIComponent(this.request.query.src || ''),		
-			lastOrigin: ides.find(_ => _.moniker === "vsc") 
-		};
-		result.isDefaultJetBrains = result.lastOrigin && result.lastOrigin.moniker.indexOf('jb-') === 0;
+			...lastOrigin
+		};	 
+		result.isDefaultJetBrains = result.lastOrigin && result.lastOrigin.moniker.indexOf('jb-') === 0; 
 		return result;
 	}
 }
