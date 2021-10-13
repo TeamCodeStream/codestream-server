325d4a6728c0ba2cb9cf48017bec7ca7ee45b50c
Colin Stryker <cstryker@newrelic.com>
2021-10-13 16:36:59 -0700

    add files

---
+++ api_server/modules/newrelic_comments/lookup_nr_org_request.js
@@ -0,0 +1,35 @@
+// handle a GET /lookup-nr-org request to lookup a New Relic org ID by account ID
+
+'use strict';
+
+const Indexes = require('./new_relic_org_indexes');
+const RestfulRequest = require(process.env.CSSVC_BACKEND_ROOT + '/api_server/lib/util/restful/restful_request');
+
+class LookupNROrgRequest extends RestfulRequest {
+
+	async authorize () {
+		// no authorization necessary, any registered user can do it
+	}
+
+	async process () {
+		const accountId = this.request.query.accountId;
+		if (!accountId) {
+			throw this.errorHandler.error('parameterRequired', { info: 'accountId '});
+		}
+		const accountIdNum = parseInt(accountId, 10);
+		if (isNaN(accountIdNum)) {
+			throw this.errorHandler.error('invalidParameter', { info: 'accountId' });
+		}
+		const record = await this.api.data.newRelicOrgs.getOneByQuery(
+			{ accountId: accountIdNum },
+			{ overrideHintRequired: true } //{ hint: Indexes.byAccountId }
+		);
+		if (!record) {
+			this.responseData = {}
+		} else {
+			this.responseData = { orgId: record.orgId };
+		}
+	}
+}
+
+module.exports = LookupNROrgRequest;
---
+++ api_server/modules/newrelic_comments/new_relic_org_indexes.js
@@ -0,0 +1,9 @@
+// indexes for the newRelicOrgs collection, mapping account IDs to org IDs
+
+'use strict';
+
+module.exports = {
+	byAccountId: {
+		accountId: 1
+	}
+};
