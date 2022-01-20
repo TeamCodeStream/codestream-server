cf0c8e2364639d11a701a3c4d36ce830766a3a03
James Price <jimmyjayp@gmail.com>
2022-01-20 11:23:06 -0500

    Disable health check logging of api in staging and qa

--- api_server/sandbox/stg.sh
+++ api_server/sandbox/stg.sh
@@ -3,6 +3,7 @@
 [ -z "$CS_API_ASSET_ENV" ] && export CS_API_ASSET_ENV=dev
 export CSSVC_ENV=stg
 export CSSVC_CONFIGURATION=codestream-cloud
+export CS_API_DONT_LOG_HEALTH_CHECKS=1
 . $CS_API_TOP/sandbox/defaults.sh
 unset CS_API_SETUP_MONGO && echo "mongo setup is disabled"
 export CS_API_INIT_FAIL_MSG="sigkill did not stop the api service"
