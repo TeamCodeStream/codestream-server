202a673ab3da386913035d3ddd66e7e85cd5abeb
Colin Stryker <cstryker@newrelic.com>
2022-01-25 08:51:57 -0800

    add password and username tests

--- api_server/modules/environment_manager/test/confirm_user/test.js
+++ api_server/modules/environment_manager/test/confirm_user/test.js
@@ -8,6 +8,8 @@ const NotFoundTest = require('./not_found_test');
 const EmailRequiredTest = require('./email_required_test');
 const NoSecretTest = require('./no_secret_test');
 const IncorrectSecretTest = require('./incorrect_secret_test');
+const UsernameTest = require('./username_test');
+const PasswordTest = require('./password_test');
 
 class ConfirmUserRequestTester {
 
@@ -17,6 +19,8 @@ class ConfirmUserRequestTester {
 		new EmailRequiredTest().test();
 		new NoSecretTest().test();
 		new IncorrectSecretTest().test();
+		new UsernameTest().test();
+		new PasswordTest().test();
 	}
 }
 
