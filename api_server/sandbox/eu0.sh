b45178ac3fa9b01704bfc5ed6c990710db4c3ba3
James Price <jimmyjayp@gmail.com>
2022-03-23 13:31:44 -0400

    Added sandbox configs for qa2 and eu0

---
+++ api_server/sandbox/eu0.sh
@@ -0,0 +1,9 @@
+
+# production EU
+[ -z "$CS_API_ASSET_ENV" ] && export CS_API_ASSET_ENV=prod
+export CSSVC_ENV=eu0
+export CSSVC_CONFIGURATION=codestream-cloud
+. $CS_API_TOP/sandbox/defaults.sh
+unset CS_API_SETUP_MONGO && echo "mongo setup is disabled"
+export CS_API_INIT_FAIL_MSG="sigkill did not stop the api service"
+export CS_API_INIT_STOP_OPTS=useForce
---
+++ inbound_email/sandbox/eu0.sh
@@ -0,0 +1,9 @@
+
+# production EU
+[ -z "$CS_MAILIN_ASSET_ENV" ] && export CS_MAILIN_ASSET_ENV=prod
+export CSSVC_ENV=eu0
+export CSSVC_CONFIGURATION=codestream-cloud
+. $CS_MAILIN_TOP/sandbox/defaults.sh || return 1
+
+# involve the system mailer in mailin service init actions
+export CS_MAILIN_MAIL_SERVICE_INIT=1
