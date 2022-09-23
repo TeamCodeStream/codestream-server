7fe26a0a932bcc3b7139acf13e722e93e9d7104e
Colin Stryker <cstryker@newrelic.com>
2022-09-23 13:34:03 -0700

    already-taken test for change email

---
+++ api_server/modules/users/test/change_email/already_taken_test.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const ChangeEmailTest = require('./change_email_test');
+const BoundAsync = require(process.env.CSSVC_BACKEND_ROOT + '/shared/server_utils/bound_async');
+
+class AlreadyTakenTest extends ChangeEmailTest {
+
+	get description () {
+		const which = this.isRegistered ? 'registered' : 'unregistered';
+		return `should return an error when submitting a request to change email and the new email already belongs to a ${which} user`;
+	}
+
+	getExpectedError () {
+		return {
+			code: 'USRC-1025'
+		};
+	}
+
+	before (callback) {
+		BoundAsync.series(this, [
+			super.before,
+			this.registerUser,
+			this.confirmUser
+		], callback);
+	}
+
+	registerUser (callback) {
+		const userData = this.userFactory.getRandomUserData();
+		userData.email = this.newEmail;
+		userData._confirmationCheat = this.apiConfig.sharedSecrets.confirmationCheat;
+		this.userFactory.registerUser(
+			userData,
+			(error, response) => {
+				if (error) { return callback(error); }
+				this.userResponse = response;
+				callback();
+			}
+		);
+	}
+
+	confirmUser (callback) {
+		if (!this.isRegistered) { return callback(); }
+		this.userFactory.confirmUser(
+			{ 
+				email: this.newEmail,
+				confirmationCode: this.userResponse.user.confirmationCode
+			},
+			callback
+		);
+	}
+}
+
+module.exports = AlreadyTakenTest;
