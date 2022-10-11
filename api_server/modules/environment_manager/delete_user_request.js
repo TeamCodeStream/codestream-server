62129bc51a56938725805d1dcfaed8710ae26643
Colin Stryker <cstryker@newrelic.com>
2022-10-11 15:37:28 -0700

    put back only deleting a user if they arent on any teams, in cross-environment join company, under one-user-per-org

--- api_server/modules/environment_manager/delete_user_request.js
+++ api_server/modules/environment_manager/delete_user_request.js
@@ -22,15 +22,9 @@ class DeleteUserRequest extends XEnvRequest {
 			throw this.errorHandler.error('alreadyDeleted');
 		}
 
-		// remove this check when we fully move to ONE_USER_PER_ORG
-		const oneUserPerOrg = (
-			this.api.modules.modulesByName.users.oneUserPerOrg ||
-			this.request.headers['x-cs-one-user-per-org']
-		);
-
 		// if not under one-user-per-org, where users must accept invites, users can only be deleted
 		// from across environments if they aren't on any teams
-		if (!oneUserPerOrg && (user.get('teamIds') || []).length > 0) {
+		if ((user.get('teamIds') || []).length > 0) {
 			throw this.errorHandler.error('deleteAuth', { reason: 'user is on at least one team' });
 		}
 		
