a6f353796fa40fb8c182aff15d76c7ce07667488
Colin Stryker <cstryker@newrelic.com>
2022-11-21 14:34:07 -0800

    test NR api call to update a user

--- api_server/modules/newrelic_idp/bot.js
+++ api_server/modules/newrelic_idp/bot.js
@@ -15,6 +15,8 @@ Commander
 	.option('-g --get <id>', 'Get New Relic / Azure user matching the given ID')
 	.option('-a --auth <domain_id>', 'List all New Relic / Azure users under given authentication domain ID')
 	.option('-u --username <username>', 'List all New Relic / Azure users matching a given username')
+	.option('--update <id>', 'Update the attributes of the user, given its ID')
+	.option('--data <data>', 'The data to use when updating user attributes, in json')
 	.parse(process.argv);
 
 (async function() {
@@ -32,6 +34,15 @@ Commander
 			response = await idp.getUsersByAuthDomain(Commander.auth);
 		} else if (Commander.username) {
 			response = await idp.getUsersByUsername(Commander.username);
+		} else if (Commander.update && Commander.data) {
+			let json;
+			try {
+				json = JSON.parse(Commander.data);
+			} catch (error) {
+				console.log('Unable to parse json:', error.message);
+				process.exit();
+			}
+			response = await idp.updateUser(Commander.update, json);
 		}
 		console.log(JSON.stringify(response && response.data, undefined, 5));
 	}
