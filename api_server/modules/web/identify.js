5a7a00e33f881412305c742441bfb99a7ed0cf4b
Colin Stryker <cstryker@newrelic.com>
2023-01-26 13:30:24 -0800

    add NR Tier to segment super properties (server side)

--- api_server/modules/web/identify.js
+++ api_server/modules/web/identify.js
@@ -44,9 +44,12 @@ module.exports = async function(options) {
 	const codeStreamOnly = company.get('linkedNROrgId') && !!(company && company.get('codestreamOnly'));
 	const orgOrigination = (company && company.get('orgOrigination')) || '';
 
-	let nrUserId, nrOrgId;
+	let nrUserId, nrOrgId, nrTier;
 	if (user.get('nrUserId')) {
-		nrOrgId = user.get('nrUserId');
+		nrUserId = user.get('nrUserId');
+	}
+	if (user.get('nrUserInfo') && user.get('nrUserInfo').userTier) {
+		nrTier = user.get('nrUserInfo').userTier;
 	}
 	if (company && company.get('linkedNROrgId')) {
 		nrOrgId = company.get('linkedNROrgId');
@@ -83,6 +86,7 @@ module.exports = async function(options) {
 		codeStreamOnly,
 		orgOrigination,
 		nrUserId,
+		nrTier,
 		nrOrgId,
 		region
 	};
