ce3a0ed8fc270cb1a7a313c9b968127aa31de090
Colin Stryker <cstryker@newrelic.com>
2023-02-28 14:54:52 -0800

    add back nr-login no-auth path

--- shared/server_utils/custom_config.js
+++ shared/server_utils/custom_config.js
@@ -455,6 +455,7 @@ module.exports = function customConfigFunc(nativeCfg) {
 		'^\\/nr-comments',
 		'^\\/xenv\\/',
 		'^\\/robots\\.txt$',
+		'^\\/~nrlogin\\/'		
 	];
 	// matching these paths means Authorization header is optional, behavior may vary
 	Cfg.apiServer.optionalAuthenticatedPaths = [
