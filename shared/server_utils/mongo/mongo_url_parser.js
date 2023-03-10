fc7d009634f5b0db833313a071d0c5a200288b92
Colin Stryker <cstryker@newrelic.com>
2023-03-10 12:23:16 -0800

    allow for mongodb dns connection format (#230)
    * allow for mongodb dns connection format
    
    * change mongo url parsing to return database only

--- shared/server_utils/mongo/mongo_url_parser.js
+++ shared/server_utils/mongo/mongo_url_parser.js
@@ -1,9 +1,21 @@
+// parse database out of a mongo url
+// here we are superseding earlier mongo url parsing (code comment out below) which parsed out a lot
+// more ... but with DNS-lookup based urls, we don't purport to return any other information besides
+// the database
+function ExtractDbFromMongoUrl (url) {
+	let parsed = url.match(/^mongodb(?:\+srv)?:\/\/([^/]+)\/([^?]+?)((\?)(.+))?$/);
+	if (parsed && parsed[2]) {
+		return parsed[2];
+	}
+}
+
+/*
 
 // a proper mongo url parser
 
 // FIXME: this should probably be moved to server_utils/mongo/mongo_client or something
 module.exports = function(mongoUrl) {
-	let parsed = mongoUrl.match(/^mongodb:\/\/([^/]+)\/([^?]+?)((\?)(.+))?$/);
+	let parsed = mongoUrl.match(/^mongodb(?:\+srv):\/\/([^/]+)\/([^?]+?)((\?)(.+))?$/);
 	const results = {
 		serversAuthString: parsed[1],
 		user: null,
@@ -32,3 +44,8 @@ module.exports = function(mongoUrl) {
 	}
 	return results;
 };
+
+*/
+
+module.exports = ExtractDbFromMongoUrl;
+
