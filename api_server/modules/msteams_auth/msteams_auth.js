ba0159befd52362da453b41aedc101352e960b39
Calvin Allen <calvinallen@newrelic.com>
2023-05-05 15:40:31 -0400

    WIP

--- api_server/modules/msteams_auth/msteams_auth.js
+++ api_server/modules/msteams_auth/msteams_auth.js
@@ -3,27 +3,15 @@
 'use strict';
 
 const OAuthModule = require(process.env.CSSVC_BACKEND_ROOT + '/api_server/lib/oauth/oauth_module.js');
-const MSTeamsAuthorizer = require('./msteams_authorizer');
 
 const OAUTH_CONFIG = {
 	provider: 'msteams',
-	host: 'login.microsoftonline.com',
-	apiHost: 'graph.microsoft.com/v1.0',
-	authPath: 'common/oauth2/v2.0/authorize',
-	tokenPath: 'common/oauth2/v2.0/token',
-	exchangeFormat: 'form',
-	scopes: [
-		'User.Read.All',
-		'Group.ReadWrite.All',
-		'offline_access'
-	].join(' '),
-	additionalAuthCodeParameters: {
-		response_mode: 'query'
-	},
-	supportsRefresh: true,
-	mockAccessTokenExpiresIn: 3600,
-	hasSharing: true
+	host: 'msteams',
+	isEnterprise: false,
+	needsConfigure: false
 };
+
+const ROUTES = [];
 
 class MSTeamsAuth extends OAuthModule {
 
@@ -32,40 +20,8 @@ class MSTeamsAuth extends OAuthModule {
 		this.oauthConfig = OAUTH_CONFIG;
 	}
 
-	// match the given MS Teams identity to a CodeStream identity
-	async getUserIdentity (options) {
-		const authorizer = new MSTeamsAuthorizer({ options });
-		return await authorizer.getMSTeamsIdentity(options.accessToken, options.providerInfo);
-	}
-
-	// an access token can be maintained for each MS Teams org
-	async getMultiAuthExtraData (info, options) {
-		let result;
-		try {
-			const authorizer = new MSTeamsAuthorizer({ options });
-			const joinedTeamsData = await authorizer.getMSTeamsJoinedTeams(info.accessToken, info);
-			const meData = await authorizer.getMSTeamsMeData(info.accessToken, info);
-			if (meData && joinedTeamsData && joinedTeamsData.teams && joinedTeamsData.teams.length) {
-				result = joinedTeamsData.teams.reduce(function (map, obj) {
-					map[obj.id] = {
-						team_name: obj.displayName,
-						team_id: obj.id,
-						user_id: meData.me && meData.me.id
-					};
-					return map;
-				}, {});
-			}
-		}
-		catch (error) {
-			options.request.warn('Request to MSTeams API failed: ' + error.message);
-			throw error;
-		}
-
-		return result;
-	}
-
-	async getUserId () {
-		return undefined;
+	getRoutes () {
+		return ROUTES;
 	}
 }
 
--- api_server/modules/providers/provider_test_constants.js
+++ api_server/modules/providers/provider_test_constants.js
@@ -170,18 +170,12 @@ const STANDARD_PROVIDER_HOSTS = {
 			'users:read.email',
 		]
 	},
-	'login*microsoftonline*com': {
-		id: 'login*microsoftonline*com',
+	'msteams': {
+        id: 'msteams',
 		name: 'msteams',
 		isEnterprise: false,
-		host: 'login.microsoftonline.com',
-		apiHost: 'graph.microsoft.com/v1.0',
-		hasSharing: true,
-		scopes: [
-			'User.Read.All',
-			'Group.ReadWrite.All',
-			'offline_access'
-		]
+		host: 'msteams',
+		needsConfigure: false
 	},
 	'okta*com': {
 		id: 'okta*com',
