65eaee59cb9e8ce2c08f205c4fe0dd1455c226f7
Calvin Allen <calvinallen@newrelic.com>
2023-05-08 13:37:57 +0000

    Simplified OAuth parts for MSTeams since we don't need it anymore

--- api_server/modules/msteams_auth/msteams_auth.js
+++ api_server/modules/msteams_auth/msteams_auth.js
@@ -7,8 +7,7 @@ const OAuthModule = require(process.env.CSSVC_BACKEND_ROOT + '/api_server/lib/oa
 const OAUTH_CONFIG = {
 	provider: 'msteams',
 	host: 'msteams',
-	isEnterprise: false,
-	needsConfigure: false
+	hasSharing: true
 };
 
 const ROUTES = [];
