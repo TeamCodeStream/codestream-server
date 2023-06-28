1b522d40ad69e953adfac22a41cdfc27b0fcf5b0
Eric Jones <etjones319@gmail.com>
2023-06-28 17:55:51 +0000

    revert unused param

--- api_server/modules/web/domain_picker_request.js
+++ api_server/modules/web/domain_picker_request.js
@@ -112,8 +112,8 @@ class DomainPickerRequest extends WebRequestBase {
 		//const redirectUri = this.getReauthRedirectUri();
 		const host = this.api.config.apiServer.marketingSiteUrl;
 		const authCompletePage = this.serviceAuth.getAuthCompletePage();
-		const redirectUri = `${host}/web/signed-in `;
-
+		const redirectUri = `${host}/auth-complete/${authCompletePage}`;
+				
 		const byInvite = this.eligibleJoinCompanies.filter(ejc => ejc.byInvite);
 		const byDomain = this.eligibleJoinCompanies.filter(ejc => ejc.byDomain);
 		const companyName = this.isWebmail ? 'My Organization' : this.domain;
