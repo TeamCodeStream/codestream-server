a1f2725eed1ae941f838f0dbd96397f3c2c3f938
Colin Stryker <cstryker@newrelic.com>
2023-07-12 14:01:51 +0000

    fix post provider set token test (which is pretty much deprecated anyway)

--- api_server/modules/providers/test/post_provider_token/existing_registered_user_test.js
+++ api_server/modules/providers/test/post_provider_token/existing_registered_user_test.js
@@ -4,10 +4,20 @@ const PostProviderTokenTest = require('./post_provider_token_test');
 
 class ExistingRegisteredUserTest extends PostProviderTokenTest {
 
-	get description () {
-		return `should be ok to set a ${this.provider} token when a registered user with matching email is already on CodeStream`;
+	constructor(options) {
+		super(options);
+		this.runRequestAsTest = true;
 	}
 
+	get description () {
+		return `should return an error when setting a ${this.provider} token when a registered user with matching email is already on CodeStream`;
+	}
+
+	getExpectedError() {
+		return {
+			code: 'USRC-1006'
+		};
+	}
 
 	setTestOptions (callback) {
 		super.setTestOptions(() => {
