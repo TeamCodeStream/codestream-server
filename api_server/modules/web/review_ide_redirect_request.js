6f2efa2a831615d196fa47551453c26c2d3e1b5f
Lexa Whitehurst <awhitehurst@newrelic.com>
2023-07-20 20:28:06 +0000

    Fix no-auth apiweb redirect page issues

--- api_server/modules/web/review_ide_redirect_request.js
+++ api_server/modules/web/review_ide_redirect_request.js
@@ -14,6 +14,7 @@ class ReviewIdeRedirectRequest extends IdeRedirectRequest {
 		this.templateProps.pageWhat = 'Review';
 		this.templateProps.analyticsContentType = 'Feedback Request';
 		if (!this.templateProps.entityId) return false;
+		else return true;
 	}
 }
 
