14e3532c2aacda106b9264bba61ab29b347987a9
Calvin Allen <calvinallen@newrelic.com>
2023-08-10 10:22:59 -0400

    Formatting and PR feedback changes

--- api_server/newrelic.js
+++ api_server/newrelic.js
@@ -6,47 +6,47 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
-  host: 'staging-collector.newrelic.com',
+	host: 'staging-collector.newrelic.com',
 
-  app_name: ['api-apm-test-calvin'],
+	app_name: ['api'],
 
-  license_key: `${process.env.CSSVC_NEWRELIC_LICENSE_KEY}`,
+	license_key: `${process.env.CSSVC_NEWRELIC_LICENSE_KEY}`,
 
-  logging: {
-    enabled: false,
-  },
+	logging: {
+		enabled: false,
+	},
 
-  code_level_metrics: {
-    enabled: true
-  },
+	code_level_metrics: {
+		enabled: true
+	},
 
-  ignore_status_codes: [403,404],
-  /**
-   * When true, all request headers except for those listed in attributes.exclude
-   * will be captured for all traces, unless otherwise specified in a destination's
-   * attributes include/exclude lists.
-   */
-  allow_all_headers: true,
-  attributes: {
-    /**
-     * Prefix of attributes to exclude from all destinations. Allows * as wildcard
-     * at end.
-     *
-     * NOTE: If excluding headers, they must be in camelCase form to be filtered.
-     *
-     * @env NEW_RELIC_ATTRIBUTES_EXCLUDE
-     */
-    exclude: [
-      'request.headers.cookie',
-      'request.headers.authorization',
-      'request.headers.proxyAuthorization',
-      'request.headers.setCookie*',
-      'request.headers.x*',
-      'response.headers.cookie',
-      'response.headers.authorization',
-      'response.headers.proxyAuthorization',
-      'response.headers.setCookie*',
-      'response.headers.x*'
-    ]
-  }
+	ignore_status_codes: [403,404],
+	/**
+	 * When true, all request headers except for those listed in attributes.exclude
+	 * will be captured for all traces, unless otherwise specified in a destination's
+	 * attributes include/exclude lists.
+	 */
+	allow_all_headers: true,
+	attributes: {
+		/**
+		 * Prefix of attributes to exclude from all destinations. Allows * as wildcard
+		 * at end.
+		 *
+		 * NOTE: If excluding headers, they must be in camelCase form to be filtered.
+		 *
+		 * @env NEW_RELIC_ATTRIBUTES_EXCLUDE
+		 */
+		exclude: [
+			'request.headers.cookie',
+			'request.headers.authorization',
+			'request.headers.proxyAuthorization',
+			'request.headers.setCookie*',
+			'request.headers.x*',
+			'response.headers.cookie',
+			'response.headers.authorization',
+			'response.headers.proxyAuthorization',
+			'response.headers.setCookie*',
+			'response.headers.x*'
+		]
+	}
 }
