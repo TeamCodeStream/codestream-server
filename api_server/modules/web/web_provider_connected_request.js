0c0f020c263bb4b4205ced0cac5efc7cb2b8beea
Lexa Whitehurst <awhitehurst@newrelic.com>
2023-09-12 17:16:33 +0000

    Add apiweb page for provider post-auth

---
+++ api_server/modules/web/web_provider_connected_request.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const WebRequestBase = require('./web_request_base');
+const WebErrors = require('./errors');
+const ProviderDisplayNames = require('./provider_display_names');
+
+class WebProviderConnectedRequest extends WebRequestBase {
+
+	async authorize () {}
+
+	async process () {
+		const providerName = ProviderDisplayNames[this.request.params.provider.toLowerCase()]
+		if (!providerName) {
+			this.warn(`Auth service ${this.provider} is not available`);
+			this.response.redirect(`/web/error?code=${WebErrors.internalError.code}&provider=${this.provider}`);
+			this.responseHandled = true;
+			return;
+		}
+		return super.render('signed_in', { newRelic: false, providerName });
+	}
+}
+
+module.exports = WebProviderConnectedRequest;
