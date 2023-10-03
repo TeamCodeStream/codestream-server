67a214cf6fcb0b9a6e29232b90d57530c35fdfeb
Calvin Allen <calvinallen@newrelic.com>
2023-10-03 14:54:30 -0400

    Add handling for possibility of sending a severity that doesn't match any of the mapping

--- shared/server_utils/simple_file_logger.js
+++ shared/server_utils/simple_file_logger.js
@@ -137,10 +137,10 @@ class SimpleFileLogger {
 		if (!this.startedOn) {
 			await this.initialize();
 			this.logAfterInitialized(text, requestId, severity, customLogProperties, json);
-			this.newRelicLogger[newRelicLevelMapper[severity || "info"]].call(this.newRelicLogger, {...customLogProperties, requestId}, text);
+			this.newRelicLogger[newRelicLevelMapper[severity || "info"] || "info"].call(this.newRelicLogger, {...customLogProperties, requestId}, text);
 		} else {
 			this.logAfterInitialized(text, requestId, severity, customLogProperties, json);
-			this.newRelicLogger[newRelicLevelMapper[severity || "info"]].call(this.newRelicLogger, {...customLogProperties, requestId}, text);
+			this.newRelicLogger[newRelicLevelMapper[severity || "info"] || "info"].call(this.newRelicLogger, {...customLogProperties, requestId}, text);
 		}
 	}
 
