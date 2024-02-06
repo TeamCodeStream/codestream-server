124ce044d52d59fbc24e45fe7ae5eba258b603e6
James Price <jimmyjayp@gmail.com>
2024-02-06 11:56:09 -0500

    devSecrets now includes config values taken from local.json

--- devSecrets.sh
+++ devSecrets.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+function dev_settings {
+    for configVar in `cat $SCRIPT_DIR/api_server/config/local.json|grep '"'|cut -f2 -d\"`; do
+        value=$(grep "\"$configVar\":" $SCRIPT_DIR/api_server/config/local.json|grep '"'|cut -f4 -d\")
+        echo "export $configVar='$value'"
+    done
+}
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export NODE_PATH=$SCRIPT_DIR/api_server/node_modules
-eval "$("$SCRIPT_DIR"/api_server/bin/cs_dev_secrets.js)"
+eval `dev_settings`
+
 unset NODE_PATH
