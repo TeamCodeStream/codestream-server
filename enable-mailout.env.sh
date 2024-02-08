2e468e7e881655cf6a3b1cc6a74e2d7717d5ccaf
James Price <jimmyjayp@gmail.com>
2024-02-08 12:34:48 -0500

    Using AWS keys from vault codestream-server/base/ for outbound email

--- enable-mailout.env.sh
+++
@@ -1,3 +0,0 @@
-export CSSVC_SUPPRESS_EMAILS=false
-export CSSVC_OUTBOUND_EMAIL_QUEUE_NAME="local_${USER}_outboundEmail"
-export CS_API_DONT_WANT_AWS=false
--- start-api-server.sh
+++ start-api-server.sh
@@ -11,15 +11,24 @@ function init_database {
 	[ $unset_after -eq 1 ] && unset STORAGE_MONGO_URL
 }
 
+function enable_mailout {
+	export CSSVC_SUPPRESS_EMAILS=false
+	export CSSVC_OUTBOUND_EMAIL_QUEUE_NAME="dev_${USER}_outboundEmail"
+	export CS_API_DONT_WANT_AWS=false
+}
+
 export CSSVC_BACKEND_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export CSSVC_ENV=local
 export NODE_PATH="$CSSVC_BACKEND_ROOT/api_server/node_modules:$CSSVC_BACKEND_ROOT/broadcaster/node_modules"
 export CSSVC_CFG_FILE="$CSSVC_BACKEND_ROOT/codestream-docker.json"
+unset AWS_PROFILE AWS_SESSION_TOKEN AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
+
 
 echo "
 CSSVC_BACKEND_ROOT=$CSSVC_BACKEND_ROOT
 CSSVC_CFG_FILE=$CSSVC_CFG_FILE
 CSSVC_ENV=$CSSVC_ENV
+AWS access will use codestream-server/base/ keys from vault
 "
 
 echo "$*" | grep -q '\-no-db' || {
@@ -29,5 +38,5 @@ echo "$*" | grep -q '\-no-db' || {
 echo "$*" | grep -q '\-init-db-only' && exit 0
 
 echo "======== Starting API ========="
-echo "$*" | grep -q '-enable-mailout' && source ./enable-mailout.env.sh && echo "API will queue outbound email to SQS $CSSVC_OUTBOUND_EMAIL_QUEUE_NAME"
+echo "$*" | grep -q '-enable-mailout' && enable_mailout && echo "API will queue outbound email to SQS $CSSVC_OUTBOUND_EMAIL_QUEUE_NAME"
 api_server/bin/api_server.js --one_worker --dev_secrets
