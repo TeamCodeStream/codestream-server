e7675484e18dc73173e64f14ffac836e9c3ce6ac
James Price <jimmyjayp@gmail.com>
2024-02-29 14:04:36 -0500

    And we bid a fond farewell to inbound email

--- inbound_email/sandbox/pi.sh
+++
@@ -1,6 +0,0 @@
-
-# use for production integration builds
-[ -z "$CS_MAILIN_ASSET_ENV" ] && export CS_MAILIN_ASSET_ENV=prod
-export CSSVC_ENV=pi
-export CSSVC_CONFIGURATION=codestream-cloud
-. $CS_MAILIN_TOP/sandbox/defaults.sh
--- inbound_email/sandbox/unified-cfg-file.sh
+++
@@ -1,3 +0,0 @@
-
-echo "the unified-cfg-file.sh sandbox configuration file is deprecated. Use defaults.sh."
-. $CS_MAILIN_TOP/sandbox/defaults.sh
